ac5dee3f1d3fc73d402f670cfcbd1fb5820d6be3
Sascha Mann <SaschaMann@users.noreply.github.com>
2017-01-23 13:35:31 +0100

    Add exercise: bob

---
+++ exercises/bob/example.jl
@@ -0,0 +1,30 @@
+function bob(stimulus::AbstractString)
+    stimulus = strip(stimulus)
+
+    if issilence(stimulus)
+        return "Fine. Be that way!"
+    elseif isshouting(stimulus)
+        return "Whoa, chill out!"
+    elseif isquestion(stimulus)
+        return "Sure."
+    else
+        return "Whatever."
+    end
+end
+
+issilence(stimulus::AbstractString) = isempty(stimulus)
+isquestion(stimulus::AbstractString) = endswith(stimulus, '?')
+
+function isshouting(stimulus::AbstractString)
+    isupper(stimulus) && return true
+    !any(isalpha, stimulus) && return false
+
+    for c in stimulus
+        # ignore all non-letter chars
+        if isalpha(c) && !isupper(c)
+            return false
+        end
+    end
+    
+    return true
+end
