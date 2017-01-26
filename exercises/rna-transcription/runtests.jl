774ca7846263f6507435fb80cf6fbe3f61acb643
Andrej Makarov <andrej.makarov@skrt.biz>
2017-01-26 09:44:28 +0000

    Add exercise: rna-transcription

--- exercises/rna-transcription/runtests.jl
+++ exercises/rna-transcription/runtests.jl
@@ -2,8 +2,39 @@ using Base.Test
 
 include("rna-transcription.jl")
 
-@testset "" begin
-  @test to_rna("") == ""
-  @test_throws ArgumentError to_rna("")
+@testset "basic transformations" begin
+	@testset "rna complement of cytosine is guanine" begin
+  	@test to_rna("C") == "G"
+	end
+
+	@testset "rna complement of guanine is cytosine" begin
+  	@test to_rna("G") == "C"
+	end
+
+	@testset "rna complement of thymine is adenine" begin
+  	@test to_rna("T") == "A"
+	end
+
+	@testset "rna complement of adenine is uracil" begin
+  	@test to_rna("A") == "U"
+	end
+end
+
+@testset "rna complement" begin
+  @test to_rna("ACGTGGTCTTAA") == "UGCACCAGAAUU"
+end
+
+@testset "error handling" begin
+	@testset "dna correctly handles invalid input" begin
+	  @test_throws ErrorException to_rna("U")
+	end
+
+	@testset "dna correctly handles completely invalid input" begin
+	  @test_throws ErrorException to_rna("XXX")
+	end
+
+	@testset "dna correctly handles partially invalid input" begin
+	  @test_throws ErrorException to_rna("ACGTXXXCTTAA")
+	end
 end
 
