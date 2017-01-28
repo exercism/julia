62cb58c9d2865e54a4e0b6c2c9a130f3c3500cbe
Andrej Makarov <andrej.makarov@skrt.biz>
2017-01-28 12:53:05 +0000

    Add exercise: word-count (#21)
    * Add exercise: word-count
    
    * Fixed runtests for word-count exercise

---
+++ exercises/word-count/example.jl
@@ -0,0 +1,9 @@
+function wordcount(sentence::AbstractString)
+  sentence = lowercase(sentence)
+  words = Dict()
+  for rx = eachmatch(r"[a-z]+'[a-z]+|[0-9a-z]+", sentence)
+    word = rx.match
+    words[word] = get(words, word, 0) + 1
+  end
+  words
+end
