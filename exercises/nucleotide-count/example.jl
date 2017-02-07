11cf2520de2e4d0e18843415c0eafc43ffe038f1
Sascha Mann <SaschaMann@users.noreply.github.com>
2017-02-07 15:04:59 +0100

    Remove hard-coded tuple of valid symbols

--- exercises/nucleotide-count/example.jl
+++ exercises/nucleotide-count/example.jl
@@ -1,7 +1,7 @@
 function count_nucleotides(strand::AbstractString)
     counter = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
     for sym in strand
-        sym in ('A', 'C', 'G', 'T') || error("Invalid nucleotide in strand")
+        sym in keys(counter) || error("Invalid nucleotide in strand")
         counter[sym] += 1
     end
     return counter
