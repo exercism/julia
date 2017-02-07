b0ede0e0a78eb68f3491eb76ef66f5817e2fb580
Andrey <xdersd@gmail.com>
2017-02-07 04:05:23 +0000

    Fix roman-numerals test set
    Added more samples, added negative number test

--- exercises/roman-numerals/runtests.jl
+++ exercises/roman-numerals/runtests.jl
@@ -2,7 +2,7 @@ using Base.Test
 
 include("roman-numerals.jl")
 
-# NOTE: Got that one from Python's testset
+# HINT: There is no need to be able to convert numbers larger than about 3000.
 samples = Dict(
     1 => "I",
     2 => "II",
@@ -21,6 +21,9 @@ samples = Dict(
     575 => "DLXXV",
     911 => "CMXI",
     1024 => "MXXIV",
+    1703 => "MDCCIII",
+    1991 => "MCMXCI",
+    2017 => "MMXVII",
     3000 => "MMM"
 )
 
@@ -30,4 +33,5 @@ end
 
 @testset "error handling" begin
     @test_throws ErrorException to_roman(0)
+    @test_throws ErrorException to_roman(-2017)
 end
