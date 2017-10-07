158728c8e604804cb5bbcfff53451a4c4b1d2826
Harrison Grodin <harrison.grodin@gmail.com>
2017-10-07 13:30:38 -0400

    Implement Grains exercise
    Contributes to exercism/julia#87. Loosely based on Python
    implementation.

---
+++ exercises/grains/grains.jl
@@ -0,0 +1,9 @@
+"""Calculate the number of grains on square `square`."""
+function on_square(square)
+
+end
+
+"""Calculate the total number of grains after square `square`."""
+function total_after(square)
+
+end
