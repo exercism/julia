09cd553435d5875220cc7c5e406d2797c7d58170
Madhav <shekharmadhav03@gmail.com>
2019-12-08 10:31:53 +0530

    debug try 1

--- exercises/acronym/example.jl
+++ exercises/acronym/example.jl
@@ -1,5 +1,5 @@
 #example.jl
-function abbreviate(words)
+function myacronym(phrase)
     regex = "[A-Z]+['a-z]*|['a-z]+"
-    return join([uppercase(word.match[1]) for word in eachmatch(Regex(regex), words)], "")
+    return join([uppercase(word.match[1]) for word in eachmatch(Regex(regex), phrase)], "")
 end
