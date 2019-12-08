3808af3ad05fb30d9ce395a134b745d33f9e16c1
Madhav <shekharmadhav03@gmail.com>
2019-12-08 22:19:13 +0530

    debug 4

--- exercises/acronym/runtests.jl
+++ exercises/acronym/runtests.jl
@@ -6,37 +6,37 @@ include("acronym.jl")
 
 
 @testset "basic" begin
-    @test myacronym("Portable Network Graphics") == "PNG"
+    @test acronym("Portable Network Graphics") == "PNG"
 end
 
 @testset "lowercase words" begin
-    @test myacronym("Ruby on Rails") == "ROR"
+    @test acronym("Ruby on Rails") == "ROR"
 end
 
 @testset "punctuation" begin
-    @test myacronym("First In, First Out") == "FIFO"
+    @test acronym("First In, First Out") == "FIFO"
 end
 
 @testset "all caps word" begin
-    @test myacronym("GNU Image Manipulation Program") == "GIMP"
+    @test acronym("GNU Image Manipulation Program") == "GIMP"
 end
 
 @testset "punctuation without whitespace" begin
-    @test myacronym("Complementary metal-oxide semiconductor") == "CMOS"
+    @test acronym("Complementary metal-oxide semiconductor") == "CMOS"
 end
 
 @testset "very long abbreviation" begin
-    @test myacronym("Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me") == "ROTFLSHTMDCOALM"
+    @test acronym("Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me") == "ROTFLSHTMDCOALM"
 end
 
 @testset "consecutive delimiters" begin
-    @test myacronym("Something - I made up from thin air") == "SIMUFTA"
+    @test acronym("Something - I made up from thin air") == "SIMUFTA"
 end
 
 @testset "apostrophes" begin
-    @test myacronym("Halley's Comet") == "HC"
+    @test acronym("Halley's Comet") == "HC"
 end
 
 @testset "underscore emphasis" begin
-    @test myacronym("The Road _Not_ Taken") == "TRNT"
+    @test acronym("The Road _Not_ Taken") == "TRNT"
 end
