4d320d8b19b65b78e59572d3f0dae6dfbc4f2c12
Ching Lam Choi <ccl5a09@gmail.com>
2019-12-31 20:20:26 +0800

    Update runtests.jl

--- exercises/matching-brackets/runtests.jl
+++ exercises/matching-brackets/runtests.jl
@@ -3,12 +3,21 @@ using Test
 include("matching-brackets.jl")
 
 @testset "Determine if brackets in expression are correctly matched" begin
-    #correctly balanced test cases
-    @test match_brackets("([])")::Bool == true  
-    @test square_of_sum("{()[]}")::Bool == true
-    @test square_of_sum("{()[()]}")::Bool == true
-    #incorrectly balanced test cases:
-    @test sum_of_squares("(")::Bool == false
-    @test sum_of_squares(")[](")::Bool == false
-    @test sum_of_squares("{([)]}")::Bool == false
+    @test match_brackets("[]")::Bool == true  
+    @test square_of_sum("")::Bool == true
+    @test square_of_sum("[[")::Bool == false
+    @test square_of_sum("}{")::Bool == false
+    @test sum_of_squares("{]")::Bool == false
+    @test sum_of_squares("{ }")::Bool == true
+    @test sum_of_squares("{[])")::Bool == false
+    @test square_of_sum("{[]}")::Bool == true
+    @test square_of_sum("{}[]")::Bool == true
+    @test square_of_sum("([{}({}[])])")::Bool == true
+    @test square_of_sum("{[)][]}")::Bool == false
+    @test square_of_sum("([{])")::Bool == false
+    @test square_of_sum("[({]})")::Bool == false
+    @test square_of_sum("{}[")::Bool == false
+    @test square_of_sum("[]]")::Bool == false
+    @test square_of_sum("(((185 + 223.85) * 15) - 543)/2")::Bool == true
+    @test square_of_sum("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")::Bool == true
 end
