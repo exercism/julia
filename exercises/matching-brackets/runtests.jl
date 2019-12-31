042d99de4f31402fb21a2464e2926fbc82b907b1
Ching Lam Choi <ccl5a09@gmail.com>
2019-12-31 20:24:20 +0800

    Update runtests.jl

--- exercises/matching-brackets/runtests.jl
+++ exercises/matching-brackets/runtests.jl
@@ -4,20 +4,20 @@ include("matching-brackets.jl")
 
 @testset "Determine if brackets in expression are correctly matched" begin
     @test match_brackets("[]")::Bool == true  
-    @test square_of_sum("")::Bool == true
-    @test square_of_sum("[[")::Bool == false
-    @test square_of_sum("}{")::Bool == false
-    @test sum_of_squares("{]")::Bool == false
-    @test sum_of_squares("{ }")::Bool == true
-    @test sum_of_squares("{[])")::Bool == false
-    @test square_of_sum("{[]}")::Bool == true
-    @test square_of_sum("{}[]")::Bool == true
-    @test square_of_sum("([{}({}[])])")::Bool == true
-    @test square_of_sum("{[)][]}")::Bool == false
-    @test square_of_sum("([{])")::Bool == false
-    @test square_of_sum("[({]})")::Bool == false
-    @test square_of_sum("{}[")::Bool == false
-    @test square_of_sum("[]]")::Bool == false
-    @test square_of_sum("(((185 + 223.85) * 15) - 543)/2")::Bool == true
-    @test square_of_sum("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")::Bool == true
+    @test match_brackets("")::Bool == true
+    @test match_brackets("[[")::Bool == false
+    @test match_brackets("}{")::Bool == false
+    @test match_brackets("{]")::Bool == false
+    @test match_brackets("{ }")::Bool == true
+    @test match_brackets("{[])")::Bool == false
+    @test match_brackets("{[]}")::Bool == true
+    @test match_brackets("{}[]")::Bool == true
+    @test match_brackets("([{}({}[])])")::Bool == true
+    @test match_brackets("{[)][]}")::Bool == false
+    @test match_brackets("([{])")::Bool == false
+    @test match_brackets("[({]})")::Bool == false
+    @test match_brackets("{}[")::Bool == false
+    @test match_brackets("[]]")::Bool == false
+    @test match_brackets("(((185 + 223.85) * 15) - 543)/2")::Bool == true
+    @test match_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")::Bool == true
 end
