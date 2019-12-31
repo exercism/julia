3572678d50fd8098ef46d547b8489447856b0415
Ching Lam Choi <ccl5a09@gmail.com>
2019-12-31 20:28:53 +0800

    Update runtests.jl

--- exercises/matching-brackets/runtests.jl
+++ exercises/matching-brackets/runtests.jl
@@ -3,21 +3,21 @@ using Test
 include("matching-brackets.jl")
 
 @testset "Determine if brackets in expression are correctly matched" begin
-    @test match_brackets("[]")::Bool == true  
-    @test match_brackets("")::Bool == true
-    @test match_brackets("[[")::Bool == false
-    @test match_brackets("}{")::Bool == false
-    @test match_brackets("{]")::Bool == false
-    @test match_brackets("{ }")::Bool == true
-    @test match_brackets("{[])")::Bool == false
-    @test match_brackets("{[]}")::Bool == true
-    @test match_brackets("{}[]")::Bool == true
-    @test match_brackets("([{}({}[])])")::Bool == true
-    @test match_brackets("{[)][]}")::Bool == false
-    @test match_brackets("([{])")::Bool == false
-    @test match_brackets("[({]})")::Bool == false
-    @test match_brackets("{}[")::Bool == false
-    @test match_brackets("[]]")::Bool == false
-    @test match_brackets("(((185 + 223.85) * 15) - 543)/2")::Bool == true
-    @test match_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")::Bool == true
+    @test matching_brackets("[]")::Bool == true  
+    @test matching_brackets("")::Bool == true
+    @test matching_brackets("[[")::Bool == false
+    @test matching_brackets("}{")::Bool == false
+    @test matching_brackets("{]")::Bool == false
+    @test matching_brackets("{ }")::Bool == true
+    @test matching_brackets("{[])")::Bool == false
+    @test matching_brackets("{[]}")::Bool == true
+    @test matching_brackets("{}[]")::Bool == true
+    @test matching_brackets("([{}({}[])])")::Bool == true
+    @test matching_brackets("{[)][]}")::Bool == false
+    @test matching_brackets("([{])")::Bool == false
+    @test matching_brackets("[({]})")::Bool == false
+    @test matching_brackets("{}[")::Bool == false
+    @test matching_brackets("[]]")::Bool == false
+    @test matching_brackets("(((185 + 223.85) * 15) - 543)/2")::Bool == true
+    @test matching_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")::Bool == true
 end
