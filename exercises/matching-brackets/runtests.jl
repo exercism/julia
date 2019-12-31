56036d874750420fcd2c14b82be05f7cf7699b56
Ching Lam Choi <ccl5a09@gmail.com>
2019-12-31 19:29:40 +0800

    Update runtests.jl

--- exercises/matching-brackets/runtests.jl
+++ exercises/matching-brackets/runtests.jl
@@ -2,21 +2,13 @@ using Test
 
 include("matching-brackets.jl")
 
-@testset "Please input an expression containing only brackets" begin
-    @test square_of_sum(5)::Integer == 225
-    @test square_of_sum(10)::Integer == 3025
-    @test square_of_sum(100)::Integer == 25502500
-end
-
-@testset "Sum the squares of the numbers up to the given number" begin
-    @test sum_of_squares(5)::Integer == 55
-    @test sum_of_squares(10)::Integer == 385
-    @test sum_of_squares(100)::Integer == 338350
-end
-
-@testset "Subtract sum of squares from square of sums" begin
-    @test difference(0)::Integer == 0
-    @test difference(5)::Integer == 170
-    @test difference(10)::Integer == 2640
-    @test difference(100)::Integer == 25164150
+@testset "Determine if brackets in expression are correctly matched" begin
+    #correctly balanced test cases
+    @test match_brackets("([])")::Bool == true  
+    @test square_of_sum("{()[]}")::Bool == true
+    @test square_of_sum("{()[()]}")::Bool == true
+    #incorrectly balanced test cases:
+    @test sum_of_squares("(")::Bool == false
+    @test sum_of_squares(")[](")::Bool == false
+    @test sum_of_squares("{([)]}")::Bool == false
 end
