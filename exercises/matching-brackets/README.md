94f43631a45d9e6252b6154bb26b481d5e0a5b96
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-01 20:22:03 +0800

    Add exercise: matching-brackets (#211)

---
+++ exercises/matching-brackets/README.md
@@ -0,0 +1,15 @@
+# Matching Brackets
+
+Given a string containing brackets `[]`, braces `{}`, parentheses `()`,
+or any combination thereof, verify that any and all pairs are matched
+and nested correctly.
+
+## Source
+
+Ginna Baker
+
+## Version compatibility
+This exercise has been tested on Julia versions >=1.0.
+
+## Submitting Incomplete Solutions
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
---
+++ exercises/matching-brackets/example.jl
@@ -0,0 +1,25 @@
+function matching_brackets(inputt)
+    inputt = split(inputt, "")
+    open_brackets,close_brackets = ["(", "[", "{"], [")", "]", "}"]
+    inputt = filter(i -> (i in union(open_brackets, close_brackets)), inputt)
+    stack = Array{String}(undef, 0)
+    brackets = Dict([("(", ")"),(")", "("), ("[", "]"), ("]", "["), ("{", "}"), ("}", "{")]) #global
+    count, valid = 0,1 #global 
+    length(inputt) % 2 != 0 && return false
+    for i in inputt
+        if i in open_brackets
+            push!(stack, i)
+            count += 1 #global
+        elseif count == 0 # & downward: assumes i not in open_brackets
+            valid = 0
+            break
+        elseif stack[end] != brackets[i] #& downward: assumes count != 0
+            valid = 0
+            break
+        else # assumes stack[end] == d[i]
+            pop!(stack)
+            count -= 1
+        end
+    end
+    return !(valid == 0 || count != 0)
+end
