85414cd279b10ea37df98ff81b0f5afe2c10e875
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-01 09:18:27 +0800

    Grouping

--- exercises/matching-brackets/runtests.jl
+++ exercises/matching-brackets/runtests.jl
@@ -1,23 +1,31 @@
+# canonical data version: 2.0.0
+
 using Test
 
 include("matching-brackets.jl")
 
 @testset "Determine if brackets in expression are correctly matched" begin
-    @test matching_brackets("[]")::Bool == true  
-    @test matching_brackets("")::Bool == true
-    @test matching_brackets("[[")::Bool == false
-    @test matching_brackets("}{")::Bool == false
-    @test matching_brackets("{]")::Bool == false
-    @test matching_brackets("{ }")::Bool == true
-    @test matching_brackets("{[])")::Bool == false
-    @test matching_brackets("{[]}")::Bool == true
-    @test matching_brackets("{}[]")::Bool == true
-    @test matching_brackets("([{}({}[])])")::Bool == true
-    @test matching_brackets("{[)][]}")::Bool == false
-    @test matching_brackets("([{])")::Bool == false
-    @test matching_brackets("[({]})")::Bool == false
-    @test matching_brackets("{}[")::Bool == false
-    @test matching_brackets("[]]")::Bool == false
-    @test matching_brackets("(((185 + 223.85) * 15) - 543)/2")::Bool == true
-    @test matching_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")::Bool == true
+    @test matching_brackets("[]") 
+    @test matching_brackets("[[")
+    @test matching_brackets("}{")
+    @test matching_brackets("{]")
+    @test matching_brackets("{ }")
+end
+
+@testset "Determine if brackets in expression are correctly matched" begin
+    @test matching_brackets("[]]")
+    @test matching_brackets("{}[")
+    @test matching_brackets("{}[]")
+    @test matching_brackets("{[])")
+    @test matching_brackets("{[]}")
+    @test matching_brackets("([{}({}[])])")
+    @test matching_brackets("{[)][]}")
+    @test matching_brackets("([{])")
+    @test matching_brackets("[({]})")
+end
+    
+@testset "Determine if brackets in expression are correctly matched" begin
+    @test matching_brackets("")
+    @test matching_brackets("(((185 + 223.85) * 15) - 543)/2")
+    @test matching_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")
 end
