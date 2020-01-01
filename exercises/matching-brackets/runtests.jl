236b2960b831f3f94d2cfc1fd625cd796265b602
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-01 19:30:49 +0800

    Update runtests.jl

--- exercises/matching-brackets/runtests.jl
+++ exercises/matching-brackets/runtests.jl
@@ -4,7 +4,7 @@ using Test
 
 include("matching-brackets.jl")
 
-@testset "Determine if brackets in expression are correctly matched" begin
+@testset "Determine if a pair of brackets are correctly matched" begin
     @test  matching_brackets("[]") 
     @test !matching_brackets("[[")
     @test !matching_brackets("}{")
@@ -12,7 +12,7 @@ include("matching-brackets.jl")
     @test  matching_brackets("{ }")
 end
 
-@testset "Determine if brackets in expression are correctly matched" begin
+@testset "Determine if 3 or more brackets are correctly matched" begin
     @test !matching_brackets("[]]")
     @test !matching_brackets("{}[")
     @test  matching_brackets("{}[]")
@@ -24,7 +24,7 @@ end
     @test !matching_brackets("[({]})")
 end
     
-@testset "Determine if brackets in expression are correctly matched" begin
+@testset "Determine if expressions with non-brackets are correctly matched" begin
     @test  matching_brackets("")
     @test  matching_brackets("(((185 + 223.85) * 15) - 543)/2")
     @test  matching_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")
