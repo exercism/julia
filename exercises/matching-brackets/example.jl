5124504b8c0e9a8a46d0e0eaec09c84271714e24
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-01 09:41:14 +0800

    Mainly style changes
    - Tabs to whitespaces
    - Var names
    - use ternary operator

--- exercises/matching-brackets/example.jl
+++ exercises/matching-brackets/example.jl
@@ -1,33 +1,29 @@
-function matching_brackets(a)
-	a = split(a, "")
-	b,c = ["(", "[", "{"], [")", "]", "}"]
-	a = filter(i -> (i in b || i in c), a)
-	stack = Array{String}(undef, 0)
-	global d, count, valid = Dict([("(", ")"),(")", "("), ("[", "]"), ("]", "["), ("{", "}"), ("}", "{")]), 0,1
-	if length(a) % 2 != 0
-		valid = 0
-	else
-		for i in a
-			if i in b
-				push!(stack, i)
-				global count += 1
-			elseif count == 0 # & downward: assumes i not in b
-				valid = 0
-				break
-			elseif stack[end] != d[i] #& downward: assumes count != 0
-				valid = 0
-				break
-			else # assumes stack[end] == d[i]
-				pop!(stack)
-				count -= 1
-			end
-		end
-	end
-	if valid == 0 || count != 0
-		return false
-		# parantheses not balanced
-	else
-		return true
-		# parantheses balanced
-	end
+function matching_brackets(inputt)
+    inputt = split(inputt, "")
+    open_brackets,close_brackets = ["(", "[", "{"], [")", "]", "}"]
+    inputt = filter(i -> (i in union(open_brackets, close_brackets)), inputt)
+    stack = Array{String}(undef, 0)
+    d = Dict([("(", ")"),(")", "("), ("[", "]"), ("]", "["), ("{", "}"), ("}", "{")]) #global
+    count, valid = 0,1 #global 
+    if length(inputt) % 2 != 0
+        valid = 0
+    else
+        for i in inputt
+            if i in open_brackets
+                push!(stack, i)
+                count += 1 #global
+            elseif count == 0 # & downward: assumes i not in open_brackets
+                valid = 0
+                break
+            elseif stack[end] != d[i] #& downward: assumes count != 0
+                valid = 0
+                break
+            else # assumes stack[end] == d[i]
+                pop!(stack)
+                count -= 1
+            end
+        end
+    end
+    (valid == 0 || count != 0) ? (return false) : (return true)
+    end
 end
