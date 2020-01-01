e862ed48db7434593aae5dd9bc22f28da3952873
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-01 09:44:12 +0800

    Update runtests.jl

--- exercises/matching-brackets/runtests.jl
+++ exercises/matching-brackets/runtests.jl
@@ -5,27 +5,27 @@ using Test
 include("matching-brackets.jl")
 
 @testset "Determine if brackets in expression are correctly matched" begin
-    @test matching_brackets("[]") 
+    @test  matching_brackets("[]") 
     @test !matching_brackets("[[")
     @test !matching_brackets("}{")
     @test !matching_brackets("{]")
-    @test matching_brackets("{ }")
+    @test  matching_brackets("{ }")
 end
 
 @testset "Determine if brackets in expression are correctly matched" begin
     @test !matching_brackets("[]]")
     @test !matching_brackets("{}[")
-    @test matching_brackets("{}[]")
+    @test  matching_brackets("{}[]")
     @test !matching_brackets("{[])")
-    @test matching_brackets("{[]}")
-    @test matching_brackets("([{}({}[])])")
+    @test  matching_brackets("{[]}")
+    @test  matching_brackets("([{}({}[])])")
     @test !matching_brackets("{[)][]}")
     @test !matching_brackets("([{])")
     @test !matching_brackets("[({]})")
 end
     
 @testset "Determine if brackets in expression are correctly matched" begin
-    @test matching_brackets("")
-    @test matching_brackets("(((185 + 223.85) * 15) - 543)/2")
-    @test matching_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")
+    @test  matching_brackets("")
+    @test  matching_brackets("(((185 + 223.85) * 15) - 543)/2")
+    @test  matching_brackets("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)")
 end
