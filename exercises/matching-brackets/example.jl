64a7c9ec069ac46fc0fa72d76096268898086f23
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-01 09:54:11 +0800

    Update example.jl

--- exercises/matching-brackets/example.jl
+++ exercises/matching-brackets/example.jl
@@ -3,15 +3,15 @@ function matching_brackets(inputt)
     open_brackets,close_brackets = ["(", "[", "{"], [")", "]", "}"]
     inputt = filter(i -> (i in union(open_brackets, close_brackets)), inputt)
     stack = Array{String}(undef, 0)
-    global d = Dict([("(", ")"),(")", "("), ("[", "]"), ("]", "["), ("{", "}"), ("}", "{")]) #global
-    global count, valid = 0,1 #global 
+    d = Dict([("(", ")"),(")", "("), ("[", "]"), ("]", "["), ("{", "}"), ("}", "{")]) #global
+    count, valid = 0,1 #global 
     if length(inputt) % 2 != 0
         valid = 0
     else
         for i in inputt
             if i in open_brackets
                 push!(stack, i)
-                global count += 1 #global
+                count += 1 #global
             elseif count == 0 # & downward: assumes i not in open_brackets
                 valid = 0
                 break
