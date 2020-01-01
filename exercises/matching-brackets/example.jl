cf4c36406bab658d218e449f61d8fd69a23ce4b6
Ching Lam Choi <ccl5a09@gmail.com>
2020-01-01 20:03:55 +0800

    Update example.jl

--- exercises/matching-brackets/example.jl
+++ exercises/matching-brackets/example.jl
@@ -5,23 +5,20 @@ function matching_brackets(inputt)
     stack = Array{String}(undef, 0)
     brackets = Dict([("(", ")"),(")", "("), ("[", "]"), ("]", "["), ("{", "}"), ("}", "{")]) #global
     count, valid = 0,1 #global 
-    if length(inputt) % 2 != 0
-        valid = 0
-    else
-        for i in inputt
-            if i in open_brackets
-                push!(stack, i)
-                count += 1 #global
-            elseif count == 0 # & downward: assumes i not in open_brackets
-                valid = 0
-                break
-            elseif stack[end] != brackets[i] #& downward: assumes count != 0
-                valid = 0
-                break
-            else # assumes stack[end] == d[i]
-                pop!(stack)
-                count -= 1
-            end
+    length(inputt) % 2 != 0 && return false
+    for i in inputt
+        if i in open_brackets
+            push!(stack, i)
+            count += 1 #global
+        elseif count == 0 # & downward: assumes i not in open_brackets
+            valid = 0
+            break
+        elseif stack[end] != brackets[i] #& downward: assumes count != 0
+            valid = 0
+            break
+        else # assumes stack[end] == d[i]
+            pop!(stack)
+            count -= 1
         end
     end
     return !(valid == 0 || count != 0)
