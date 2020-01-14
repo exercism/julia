b98935117b0205ad9d4272af33e1a88601ae05f6
akshat <akshatmehrotra2004@gmail.com>
2020-01-14 19:28:32 +0530

    added perfect numbers
    I have'nt added it to the config file as i am unaware how to.

---
+++ exercises/perfect-numbers/example.jl
@@ -0,0 +1,21 @@
+function classify(number)
+    factors = []
+    if number == 1
+        return "deficient"
+    elseif number <= 0
+        return "Classification is only possible for natural numbers."
+    end
+    
+    for i in 1:(number-1)
+        if number%i == 0
+            push!(factors, i)
+        end
+    end
+    if sum(factors) > number 
+        return "abundant"
+    elseif sum(factors) < number 
+        return "deficient"
+    else
+        return "perfect"
+    end
+end
