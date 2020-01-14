24ee05375996fd0f23ecc0b13a629d23ebf0f7f7
akshat <akshatmehrotra2004@gmail.com>
2020-01-14 23:44:23 +0530

    Update example.jl

--- exercises/perfect-numbers/example.jl
+++ exercises/perfect-numbers/example.jl
@@ -1,3 +1,6 @@
+## the line bellow has nothing to do with the code
+using Pkg; Pkg.add("LazyJSON")
+
 function classify(number)
     factors = []
     if number == 1
@@ -5,15 +8,15 @@ function classify(number)
     elseif number <= 0
         return "Classification is only possible for natural numbers."
     end
-    
+
     for i in 1:(number-1)
         if number%i == 0
             push!(factors, i)
         end
     end
-    if sum(factors) > number 
+    if sum(factors) > number
         return "abundant"
-    elseif sum(factors) < number 
+    elseif sum(factors) < number
         return "deficient"
     else
         return "perfect"
