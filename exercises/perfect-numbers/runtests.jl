d8dfd5523cd14dbf9d7f16fd2907f8603b881eeb
akshat <akshatmehrotra2004@gmail.com>
2020-01-14 22:49:11 +0530

    fixed tests and added readme

--- exercises/perfect-numbers/runtests.jl
+++ exercises/perfect-numbers/runtests.jl
@@ -1,1 +1,18 @@
-{\rtf1}
+# canonical version = 1.3.0
+using Test
+using LazyJSON
+include("example.jl")
+
+j = LazyJSON.value(String(read("canonical-data.json")))
+
+for i in j["cases"]
+    for j in i["cases"]
+        des = String(j["description"])
+        inp = j["input"]["number"]
+
+        i["description"] == "Invalid inputs" ? exped = j["expected"]["error"] : exped = j["expected"]
+        @testset "$des" begin
+           @test classify(inp) == exped
+        end
+    end
+end
