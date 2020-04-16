8c740f09b6ae583cffcb7081d641933c09cc67c4
Oliver Blanthorn <freedom4cows@gmail.com>
2020-04-16 13:19:34 +0100

    Fix swapped function docstrings

--- exercises/difference-of-squares/difference-of-squares.jl
+++ exercises/difference-of-squares/difference-of-squares.jl
@@ -1,9 +1,9 @@
-"Sum the squares of the numbers up to the given number"
+"Square the sum of the numbers up to the given number"
 function square_of_sum(n::Int)
 
 end
 
-"Square the sum of the numbers up to the given number"
+"Sum the squares of the numbers up to the given number"
 function sum_of_squares(n::Int)
 
 end
