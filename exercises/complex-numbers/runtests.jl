52f3d42ea59bb3970b80da8685e2c798bc441411
Oliver Blanthorn <freedom4cows@gmail.com>
2020-04-18 12:09:04 +0100

    complex-numbers: Add leeway for floating point rounding issues (#223)

--- exercises/complex-numbers/runtests.jl
+++ exercises/complex-numbers/runtests.jl
@@ -30,7 +30,7 @@ include("complex-numbers.jl")
     @testset "Division" begin
         @test ComplexNumber(1, 0) / ComplexNumber(2, 0) == ComplexNumber(0.5, 0)
         @test ComplexNumber(0, 1) / ComplexNumber(0, 2) == ComplexNumber(0.5, 0)
-        @test ComplexNumber(1, 2) / ComplexNumber(3, 4) == ComplexNumber(0.44, 0.08)
+        @test ComplexNumber(1, 2) / ComplexNumber(3, 4) ≈ ComplexNumber(0.44, 0.08)
     end
 end
 
