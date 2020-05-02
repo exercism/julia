a934f7bbe4af124db24f369c4022345819e17c1c
Colin Caine <cmcaine@gmail.com>
2020-05-02 01:23:29 +0100

    Arguably improve the docstrings for diff of sqs
    The previous version pluralised "sums" when it should not have, and then
    I got carried away.

--- exercises/difference-of-squares/difference-of-squares.jl
+++ exercises/difference-of-squares/difference-of-squares.jl
@@ -1,14 +1,14 @@
-"Square the sum of the numbers up to the given number"
+"Square the sum of the first `n` positive integers"
 function square_of_sum(n::Int)
 
 end
 
-"Sum the squares of the numbers up to the given number"
+"Sum the squares of the first `n` positive integers"
 function sum_of_squares(n::Int)
 
 end
 
-"Subtract sum of squares from square of sums"
+"Subtract the sum of squares from square of the sum of the first `n` positive ints"
 function difference(n::Int)
 
 end
