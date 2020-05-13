b4abfa97ef05bf6d4cc53404183f8aa09890f255
Colin Caine <cmcaine@gmail.com>
2020-05-13 17:29:41 +0100

    perfect-numbers: fix example for Julia <1.4 #232

--- exercises/perfect-numbers/example.jl
+++ exercises/perfect-numbers/example.jl
@@ -1,6 +1,10 @@
 function aliquot_sum(n)
     if !isinteger(n) || n < 0
         throw(DomainError(n,"n must be a non-negative integer"))
+    # In Julia <1.4, you need this special case because `sum` doesn't like
+    # empty collections
+    elseif n <= 1
+        return 0
     end
     return sum(i for i in 1:div(n, 2) if n % i == 0)
 end
