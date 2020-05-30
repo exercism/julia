cc1478ea963e2e31cf0cfd69a0ebdff8e6d89af3
Colin Caine <cmcaine@gmail.com>
2020-05-30 21:29:20 +0000

    Remove type annotations from hamming
    Type annotations are baaad.
    
    In this case, for common solutions, the code will work for any iterable,
    but with type annotations it will be artificially restricted to strings.
    That's not clever.
    
    Updated example.jl slightly while I was here, because `count` is a bit
    nicer than mapreduce.

--- exercises/hamming/example.jl
+++ exercises/hamming/example.jl
@@ -1,4 +1,4 @@
-function distance(s1::AbstractString, s2::AbstractString)
+function distance(s1, s2)
     length(s1) != length(s2) && throw(ArgumentError("Sequences must have the same length"))
-    reduce(+, a != b for (a, b) in zip(s1, s2); init=0)
+    count(a != b for (a, b) in zip(s1, s2))
 end
