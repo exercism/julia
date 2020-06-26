6ccccb078bdd45bf1af73ed38c5817144a772cae
Vincent Yu <v@v-yu.com>
2020-06-26 10:30:29 +0000

    Modify diff-generated-runtests.sh to use local copy of problem-specifications/

--- bin/diff-generated-runtests.sh
+++ bin/diff-generated-runtests.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-# Run from top-level project directory.
+# The problem-specifications directory shoud be adjacent to this project's root directory;
+# otherwise you must provide its path as an argument to this script.
 #
 # For each generated slug listed below, this script downloads canonical-data.json and diffs
 # the existing runtests.jl with a newly generated one. It does not modify existing files or
@@ -9,26 +10,31 @@
 # Update as necessary.
 slugs=(circular-buffer darts)
 
-if ! [[ -d exercises/ ]]; then
-  echo "Error: ./exercises/ does not exist." >&2; exit 1
-elif ! [[ -d generators/ ]]; then
-  echo "Error: ./generators/ does not exist." >&2; exit 1
+project_dir="$(dirname "$0")/../"
+
+if (( $# > 0 )); then
+    data_dir="$1/exercises/"
+else
+    data_dir="$project_dir/../problem-specifications/exercises/"
 fi
 
-echo 'Generating new runtests and checking each for differences with existing runtests.jl' >&2
+if ! [[ -d $data_dir ]]; then
+    echo "Error: $data_dir does not exist." >&2; exit 1
+fi
+
+echo 'Generating new runtests and diffing each with the existing runtests.jl.' >&2
 count=0
 for slug in ${slugs[@]}; do
-  (( count += 1 ))
-  echo "$slug" >&2
-  url="https://raw.githubusercontent.com/exercism/problem-specifications/master/exercises/$slug/canonical-data.json"
-  wget -q -O - "$url" \
-    | JULIA_LOAD_PATH=:generators/GeneratorUtils/ julia --project=generators/ "generators/$slug.jl" \
-    | diff "exercises/$slug/runtests.jl" -
+    (( count += 1 ))
+    echo "$slug" >&2
+    < "$data_dir/$slug/canonical-data.json" \
+        julia --project="$project_dir/generators/" "$project_dir/generators/$slug.jl" \
+        | diff "$project_dir/exercises/$slug/runtests.jl" -
 
-  if (( $? != 0 )); then
-    echo "$slug: Generated runtests differs from existing runtests.jl." >&2
-    exit 1
-  fi
+    if (( $? != 0 )); then
+        echo "$slug: Generated runtests differs from existing runtests.jl." >&2
+        exit 1
+    fi
 done
 
 echo "All $count slugs checked and passed (no differences)." >&2
