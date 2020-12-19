b7627106b09cac4be9fddc816f0139edc616a7c5
Sascha Mann <git@mail.saschamann.eu>
2020-12-19 02:25:36 +0100

    isogram: add missing tests

--- exercises/isogram/runtests.jl
+++ exercises/isogram/runtests.jl
@@ -33,3 +33,27 @@ end
 @testset "made-up name that is an isogram" begin
     @test isisogram("Emily Jung Schwartzkopf")
 end
+
+@testset "word with one duplicated character from the end of the alphabet" begin
+    @test !isisogram("zzyzx")
+end
+
+@testset "word with duplicated character in mixed case, lowercase first" begin
+    @test !isisogram("alphAbet")
+end
+
+@testset "hypothetical word with duplicated character following hyphen" begin
+    @test !isisogram("thumbscrew-jappingly")
+end
+
+@testset "isogram with duplicated hyphen" begin
+    @test isisogram("six-year-old")
+end
+
+@testset "duplicated character in the middle" begin
+    @test !isisogram("accentor")
+end
+
+@testset "same first and last characters" begin
+    @test !isisogram("angola")
+end
