7c45d42648d5b8ac0da0a0ae9426ef103c5a73bf
Sascha Mann <git@mail.saschamann.eu>
2020-12-19 01:57:24 +0100

    armstrong-numbers: add missing tests

--- exercises/armstrong-numbers/runtests.jl
+++ exercises/armstrong-numbers/runtests.jl
@@ -5,9 +5,11 @@ using Test
 include("armstrong-numbers.jl")
 
 @testset "armstrong numbers" begin
+    @test  isarmstrong(0)
     @test  isarmstrong(5)
     @test !isarmstrong(10)
     @test !isarmstrong(100)
+    @test  isarmstrong(153)
     @test  isarmstrong(9474)
     @test !isarmstrong(9475)
     @test  isarmstrong(9926315)
