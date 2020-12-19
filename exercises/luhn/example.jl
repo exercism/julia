66f5455e9fbe97b20668d402b0fad89c75fabac7
Colin Caine <cmcaine@gmail.com>
2020-12-19 12:11:37 +0000

    Improve example for luhn exercise
    This is a lot more readable, imo. It's more efficient, too, though we
    can fairly easily go 25x faster than this one.

--- exercises/luhn/example.jl
+++ exercises/luhn/example.jl
@@ -1,18 +1,30 @@
-function luhn(id::AbstractString)
-    id = split(replace(id, ' ' => ""), "")
-    length(id) < 2 && return false
-    all(all(isdigit, s) for s in id) || return false
+"""
+    luhn(str)
 
-    numbers = Int[]
-    for val in id
-        push!(numbers, parse(Int, val))
-    end
+Return true if `str` encodes a valid number in the luhn formula.
 
-    for i in length(numbers) - 1:-2:1
-        if numbers[i] != 9
-            numbers[i] = (numbers[i] * 2) % 9
+A valid string will:
+
+ - Contain only 0-9 and space
+ - Contain at least two numbers
+ - After a transformation, the sum of the transformed digits will be evenly divisible by 10
+
+The transformation is:
+
+ - Every second digit, counting the rightmost digit as first, is doubled
+ - If a doubled digit is greater than 9, subtract 9 from it.
+"""
+function luhn(str)
+    acc = 0
+    len = 0
+    for chr in Iterators.reverse(str)
+        if isdigit(chr)
+            len += 1
+            x = parse(Int, chr)
+            acc += iseven(len) ? (2x < 10 ? 2x : 2x-9) : x
+        elseif chr != ' '
+            return false
         end
     end
-
-    return sum(numbers) % 10 == 0
+    len > 1 ? acc % 10 == 0 : false
 end
