36e1aa7ae51cced772c1e92a16234f87575d69e2
Sascha Mann <git@mail.saschamann.eu>
2020-12-19 10:46:59 +0100

    rna-transcription: add missing tests
    rna-transcription: fix failing test

--- exercises/rna-transcription/example.jl
+++ exercises/rna-transcription/example.jl
@@ -1,6 +1,7 @@
 # Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement:
 # G -> C, C -> G, T -> A, A -> U
 function to_rna(dna::AbstractString)
+    isempty(dna) && return ""
     typeof(match(r"^[GCTA]+$", dna)) == Nothing && error("Invalid RNA strand")
     # Define character associations
     a_nucleotides = Dict('G'=>'C', 'C'=>'G', 'T'=>'A', 'A'=>'U')
--- exercises/rna-transcription/runtests.jl
+++ exercises/rna-transcription/runtests.jl
@@ -3,20 +3,24 @@ using Test
 include("rna-transcription.jl")
 
 @testset "basic transformations" begin
+    @testset "empty rna sequence" begin
+        @test to_rna("") == ""
+    end
+
     @testset "rna complement of cytosine is guanine" begin
-      @test to_rna("C") == "G"
+        @test to_rna("C") == "G"
     end
 
     @testset "rna complement of guanine is cytosine" begin
-      @test to_rna("G") == "C"
+        @test to_rna("G") == "C"
     end
 
     @testset "rna complement of thymine is adenine" begin
-      @test to_rna("T") == "A"
+        @test to_rna("T") == "A"
     end
 
     @testset "rna complement of adenine is uracil" begin
-      @test to_rna("A") == "U"
+        @test to_rna("A") == "U"
     end
 end
 
@@ -26,14 +30,14 @@ end
 
 @testset "error handling" begin
     @testset "dna correctly handles invalid input" begin
-      @test_throws ErrorException to_rna("U")
+        @test_throws ErrorException to_rna("U")
     end
 
     @testset "dna correctly handles completely invalid input" begin
-      @test_throws ErrorException to_rna("XXX")
+        @test_throws ErrorException to_rna("XXX")
     end
 
     @testset "dna correctly handles partially invalid input" begin
-      @test_throws ErrorException to_rna("ACGTXXXCTTAA")
+        @test_throws ErrorException to_rna("ACGTXXXCTTAA")
     end
 end
