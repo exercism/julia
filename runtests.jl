74650168b7effb4aee3e395048cb552d6747a88b
Sascha Mann <git@mail.saschamann.eu>
2021-03-06 16:08:14 +0100

    Update runtests.jl

--- runtests.jl
+++ runtests.jl
@@ -26,7 +26,7 @@ for exercise_type in ("concept", "practice")
         # runtests.jl includes the solution by calling `include("slug.jl")`
         # Our anonymous module doesn't have `include(s::String)` defined,
         # so we define our own.
-        Core.eval(m, :(include(s) = $(Base.include)($m, $(joinpath(exercise_path, ".meta", example_or_exemplar * ".jl")))
+        Core.eval(m, :(include(s) = $(Base.include)($m, $(joinpath(exercise_path, ".meta", example_or_exemplar * ".jl")))))
         @info "[$(uppercase(exercise_type))] Testing $exercise"
         Base.include(m, joinpath(exercise_path, "runtests.jl"))
         
