68acf52d5b2c24dd4b793960636f5346fe9f5254
Sascha Mann <git@mail.saschamann.eu>
2021-03-13 16:22:38 +0100

    annalyns-infiltration: Add exemplar docstrings

--- exercises/concept/annalyns-infiltration/.meta/exemplar.jl
+++ exercises/concept/annalyns-infiltration/.meta/exemplar.jl
@@ -1,4 +1,27 @@
+"""
+    can_do_fast_attack(knight_awake)
+
+Return if a fast attack is possible.
+"""
 can_do_fast_attack(knight_awake) = !knight_awake
+
+"""
+    can_spy(knight_awake, archer_awake, prisoner_awake)
+
+Return if the spy action is possible.
+"""
 can_spy(knight_awake, archer_awake, prisoner_awake) = knight_awake || archer_awake || prisoner_awake
+
+"""
+    can_signal_prisoner(archer_awake, prisoner_awake)
+
+Return if signaling the prisoner is possible.
+"""
 can_signal_prisoner(archer_awake, prisoner_awake) = prisoner_awake && !archer_awake
+
+"""
+    can_free_prisoner(knight_awake, archer_awake, prisoner_awake, dog_present)
+
+Return if the prisoner can be freed.
+"""
 can_free_prisoner(knight_awake, archer_awake, prisoner_awake, dog_present) = !knight_awake && !archer_awake && prisoner_awake || dog_present && !archer_awake
