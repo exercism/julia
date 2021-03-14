c07b5c6c6d2953dac9a9b84af66eeefddbb45d0c
Sascha Mann <git@mail.saschamann.eu>
2021-03-14 15:11:07 +0100

    Remove lasagna docstrings
    This will be a separate exercise

--- exercises/concept/lasagna/.meta/exemplar.jl
+++ exercises/concept/lasagna/.meta/exemplar.jl
@@ -1,20 +1,3 @@
-"""
-    preptime(layers)
-
-Return the preparation times in minutes.
-"""
 preptime(layers) = 2layers
-
-"""
-    remaining_time(current_time)
-
-Return the remaining oven time in minutes.
-"""
 remaining_time(current_time) = 60 - current_time
-
-"""
-    total_working_time(layers, current_time)
-
-Return the total working time in minutes.
-"""
 total_working_time(layers, current_time) = preptime(layers) + current_time
