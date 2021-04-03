1e267d82c269787c8b8e33bd24a4d49df1fa249e
Sascha Mann <git@mail.saschamann.eu>
2021-04-03 17:52:50 +0200

    CI: Respect version requirement of exercises (#363)
    * CI: Respect version requirement of exercises
    
    * Add artifact caching

--- runtests.jl
+++ runtests.jl
@@ -1,4 +1,11 @@
+using JSON
+using Pkg
 using Test
+
+# Setup GHA logger in CI
+using Logging: global_logger
+using GitHubActions: GitHubActionsLogger
+get(ENV, "GITHUB_ACTIONS", "false") == "true" && global_logger(GitHubActionsLogger())
 
 for exercise_type in ("concept", "practice")
     for exercise in readdir(joinpath("exercises", exercise_type))
@@ -9,6 +16,15 @@ for exercise_type in ("concept", "practice")
 
         exercise_path = joinpath("exercises", exercise_type, exercise)
         isdir(exercise_path) || continue
+
+        # Skip exercise tests if the Julia version doesn't meet the required version as specified in .meta/config.json
+        cfg = JSON.parsefile(joinpath(exercise_path, ".meta", "config.json"))
+        required_version_spec = Pkg.Types.semver_spec(get(cfg, "language_versions", "1.0"))
+        if VERSION ∉ required_version_spec
+            @info "$exercise requires Julia $required_version_spec, skipping tests."
+            println()
+            continue
+        end
 
         # Concept exercises have an exemplar, practice exercises merely an example for CI purposes
         example_or_exemplar = exercise_type == "concept" ? "exemplar" : "example"
