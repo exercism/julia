fdd4698cd76d15d90138a596a779ad35c7bdf264
Sascha Mann <git@mail.saschamann.eu>
2021-04-06 08:32:13 +0200

    Add exercise: elyses-analytic-enchantments / predicates (#367)
    * Add exercise: elyses-analytic-enchantments / predicates
    
    The point of this exercise is to introduce several Base methods to analyse the contents of vectors, and to familiarise the notion of predicate functions to students.
    
    * Use isnothing
    
    * Apply suggestions from code review
    
    Co-authored-by: Colin Caine <cmcaine@gmail.com>
    
    * Move some hints to general section
    
    * Tell students to return nothing if card not in stack
    
    * Add blurb
    
    * Fix json
    
    * Update exercises/concept/elyses-analytic-enchantments/.meta/config.json
    
    Co-authored-by: Colin Caine <cmcaine@gmail.com>

---
+++ exercises/concept/elyses-analytic-enchantments/.meta/exemplar.jl
@@ -0,0 +1,50 @@
+# Julia 1.0 compat
+if VERSION < v"1.1"
+    @eval isnothing(::Any) = false
+    @eval isnothing(::Nothing) = true
+end
+
+"""
+    has_card(stack, card)
+
+Return true if `stack` contains `card`.
+"""
+has_card(stack, card) = card ∈ stack
+
+"""
+    find_card(stack, card)
+
+Return the index of `card` in `stack`.
+"""
+find_card(stack, card) = findfirst(==(card), stack)
+
+"""
+    all_cards_even(stack)
+
+Return true if all cards in `stack` are even.
+"""
+all_cards_even(stack) = all(iseven, stack)
+
+"""
+    any_odd_cards(stack)
+
+Return true if any cards in `stack` are odd.
+"""
+any_odd_cards(stack) = any(isodd, stack)
+
+"""
+    first_even_card_idx(stack)
+
+Return the index of the first even card in `stack`.
+"""
+first_even_card_idx(stack) = findfirst(iseven, stack)
+
+"""
+    first_odd_card(stack)
+
+Return the first odd card in `stack`, and `nothing` if the stack does not contain any odd cards.
+"""
+function first_odd_card(stack)
+    idx = findfirst(isodd, stack)
+    isnothing(idx) ? nothing : stack[idx]
+end
