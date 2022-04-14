1d087424d364ed37845d0d4431ec10bc88e840fe
Sascha Mann <git@mail.saschamann.eu>
2022-04-14 10:33:08 +0200

    roman-numerals: update to latest spec

--- exercises/practice/roman-numerals/runtests.jl
+++ exercises/practice/roman-numerals/runtests.jl
@@ -10,21 +10,26 @@ samples = Dict(
     5 => "V",
     6 => "VI",
     9 => "IX",
+    16 => "XVI",
     27 => "XXVII",
     48 => "XLVIII",
     49 => "XLIX",
     59 => "LIX",
+    66 => "LXVI",
     93 => "XCIII",
     141 => "CXLI",
     163 => "CLXIII",
+    166 => "CLXVI",
     402 => "CDII",
     575 => "DLXXV",
+    666 => "DCLXVI",
     911 => "CMXI",
     1024 => "MXXIV",
+    1666 => "MDCLXVI",
     1703 => "MDCCIII",
     1991 => "MCMXCI",
     2017 => "MMXVII",
-    3000 => "MMM"
+    3000 => "MMM",
 )
 
 @testset "convert $(sample[1]) to roman numeral" for sample in samples
