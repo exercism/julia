3de427863d9f5222b84f3a9e3b4c704d81c1b181
Sascha Mann <git@mail.saschamann.eu>
2022-04-14 10:37:35 +0200

    prime-factors: update to latest spec

--- exercises/practice/prime-factors/runtests.jl
+++ exercises/practice/prime-factors/runtests.jl
@@ -9,16 +9,36 @@ end
     @test prime_factors(2) == [2]
 end
 
+@testset "another prime number" begin
+    @test prime_factors(3) == [3]
+end
+
+@testset "product of first prime" begin
+    @test prime_factors(4) == [2, 2]
+end
+
 @testset "square of a prime" begin
     @test prime_factors(9) == [3, 3]
+end
+
+@testset "product of first and second prime" begin
+    @test prime_factors(6) == [2, 3]
 end
 
 @testset "cube of a prime" begin
     @test prime_factors(8) == [2, 2, 2]
 end
 
+@testset "product of second prime" begin
+    @test prime_factors(27) == [3, 3, 3]
+end
+
 @testset "product of primes and non-primes" begin
     @test prime_factors(12) == [2, 2, 3]
+end
+
+@testset "product of third prime" begin
+    @test prime_factors(625) == [5, 5, 5, 5]
 end
 
 @testset "product of primes" begin
