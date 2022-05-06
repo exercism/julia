9bdf9cfe7bf5fe6e03083d6ab776bc0676fb8d1c
Colin Caine <cmcaine@gmail.com>
2022-05-06 18:53:59 +0100

    Change problem definition
    As I wrote in https://github.com/exercism/julia/issues/503
    
    Thinking about it some more, I think that's because `isvalid` isn't really
    the right function for us. `isvalid` checks if a string or character
    contains only valid UTF-8 codepoints. I think it only makes sense for types
    where the representation of the type (the bits in memory) can encode
    something invalid in normal use. This is the case for strings because Julia
    does not validate that strings or characters are correctly encoded when
    they are created, it just treats them as opaque bytes.
    
    For most Julia types, however, you cannot create an invalid value of the
    type, except through silly use of pointers.
    
    I think in real code the advice would be to parse the string into an ISBN
    type, and if that is not possible (e.g. because the string does not encode
    a valid ISBN), the ISBN constructor (or `parse(ISBN, s)`, whichever makes
    more sense) should throw an error. If a non-throwing option is required,
    `tryparse(ISBN, s)` is the right function to define.
    
    Personally, I would define only a constructor and have it throw errors.

--- exercises/practice/isbn-verifier/runtests.jl
+++ exercises/practice/isbn-verifier/runtests.jl
@@ -2,64 +2,18 @@ using Test
 
 include("isbn-verifier.jl")
 
-@test ISBN <: AbstractString
-
-@testset "valid ISBN numbers" begin
+@testset "valid ISBNs are canonicalised" begin
     # ISBN number
-    @test isvalid(ISBN, "3-598-21508-8")
+    @test value(ISBN("3-598-21508-8")) == "3598215088"
     # ISBN number with a check digit of 10
-    @test isvalid(ISBN, "3-598-21507-X")
+    @test value(ISBN("3-598-21507-X")) == "359821507X"
     # ISBN without separating dashes
-    @test isvalid(ISBN, "3598215088")
+    @test value(ISBN("3598215088")) == "3598215088"
     # ISBN without separating dashes and X as check digit
-    @test isvalid(ISBN, "359821507X")
+    @test value(ISBN("359821507X")) == "359821507X"
 end
 
-@testset "invalid ISBN numbers" begin
-    # invalid ISBN check digit
-    @test !isvalid(ISBN, "3-598-21508-9")
-    # check digit is a character other than X
-    @test !isvalid(ISBN, "3-598-21507-A")
-    # invalid character in ISBN
-    @test !isvalid(ISBN, "3-598-2K507-0")
-    # X is only valid as a check isdigit
-    @test !isvalid(ISBN, "3-598-2X507-9")
-    # ISBN without check digit and dashes
-    @test !isvalid(ISBN, "359821507")
-    # too long ISBN and no dashes
-    @test !isvalid(ISBN, "3598215078X")
-    # ISBN without check digit
-    @test !isvalid(ISBN, "3-598-21507")
-    # too long ISBN
-    @test !isvalid(ISBN, "3-598-21507-XX")
-    # check digit of X should not be used for 0
-    @test !isvalid(ISBN, "3-598-21515-X")
-    # empty ISBN
-    @test !isvalid(ISBN, "")
-    # invalid character in ISBN
-    @test !isvalid(ISBN, "3-598-P1581-X")
-    # too short ISBN
-    @test !isvalid(ISBN, "00")
-    # input is 9 characters
-    @test !isvalid(ISBN, "134456729")
-    # invalid characters are not ignored
-    @test !isvalid(ISBN, "3132P34035")
-    # input is too long but contains a valid ISBN
-    @test !isvalid(ISBN, "98245726788")
-end
-
-@testset "constructing valid ISBN numbers" begin
-    # ISBN number
-    @test string(isbn"3-598-21508-8") == "3598215088"
-    # ISBN number with a check digit of 10
-    @test string(isbn"3-598-21507-X") == "359821507X"
-    # ISBN without separating dashes
-    @test string(isbn"3598215088") == "3598215088"
-    # ISBN without separating dashes and X as check digit
-    @test string(isbn"359821507X") == "359821507X"
-end
-
-@testset "constructing invalid ISBN numbers" begin
+@testset "invalid ISBNs throw DomainError" begin
     # invalid ISBN check digit
     @test_throws DomainError ISBN("3-598-21508-9")
     # check digit is a character other than X
@@ -90,4 +44,8 @@ end
     @test_throws DomainError ISBN("3132P34035")
     # input is too long but contains a valid ISBN
     @test_throws DomainError ISBN("98245726788")
+end
+
+@testset "isbn_str macro works" begin
+    @test ISBN("359821507X") == isbn"359821507X"
 end
