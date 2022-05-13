9107153976f895f2a3a6556ff4c344d9fb868d2f
gpucce <g.puccetti92@gmail.com>
2022-05-14 00:39:26 +0200

    format

--- exercises/practice/binary-search/runtests.jl
+++ exercises/practice/binary-search/runtests.jl
@@ -8,8 +8,7 @@ include("binary-search.jl")
         @test binarysearch([1, 3, 4, 6, 8, 9, 11], 6) == 4:4
         @test binarysearch([1, 3, 4, 6, 8, 9, 11], 1) == 1:1
         @test binarysearch([1, 3, 4, 6, 8, 9, 11], 11) == 7:7
-        @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144) ==
-              10:10
+        @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144) == 10:10
         @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 21) == 6:6
     end
     @testset "value not in array" begin
@@ -31,16 +30,8 @@ if @isdefined(enable_bonus_tasks) && enable_bonus_tasks
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 6, rev = true) == 4:4
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 1, rev = true) == 7:7
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 11, rev = true) == 1:1
-                @test binarysearch(
-                    [634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    144,
-                    rev = true,
-                ) == 4:4
-                @test binarysearch(
-                    [377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    21,
-                    rev = true,
-                ) == 7:7
+                @test binarysearch([634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1], 144, rev = true,) == 4:4
+                @test binarysearch([377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],21,rev = true,) == 7:7
             end
             @testset "value not in array" begin
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 7, rev = true) == 4:3
@@ -53,35 +44,17 @@ if @isdefined(enable_bonus_tasks) && enable_bonus_tasks
         @testset "apply transformation" begin
             @testset "value in array" begin
                 @test binarysearch([5.5], 6, by = round) == 1:1
-                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 6, by = round) ==
-                      4:4
-                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 1, by = round) ==
-                      1:1
-                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 11, by = round) ==
-                      7:7
-                @test binarysearch(
-                    [1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8],
-                    11,
-                    by = abs2 ∘ round,
-                ) == 7:7
-                @test binarysearch(
-                    [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634],
-                    144.4,
-                    by = round,
-                ) == 10:10
-                @test binarysearch(
-                    [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377],
-                    20.6,
-                    by = round,
-                ) == 6:6
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 6, by = round) == 4:4
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 1, by = round) == 1:1
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 11, by = round) == 7:7
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 11, by = abs2 ∘ round,) == 7:7
+                @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144.4, by = round,) == 10:10
+                @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 20.6, by = round,) == 6:6
             end
             @testset "value not in array" begin
-                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 7, by = round) ==
-                      5:4
-                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 0, by = round) ==
-                      1:0
-                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 13, by = round) ==
-                      8:7
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 7, by = round) == 5:4
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 0, by = round) == 1:0
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 13, by = round) == 8:7
                 @test binarysearch([], 1, by = round) == 1:0
             end
         end
@@ -93,16 +66,8 @@ if @isdefined(enable_bonus_tasks) && enable_bonus_tasks
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 6, lt = >) == 4:4
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 1, lt = >) == 7:7
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 11, lt = >) == 1:1
-                @test binarysearch(
-                    [634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    144,
-                    lt = >,
-                ) == 4:4
-                @test binarysearch(
-                    [377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    21,
-                    lt = >,
-                ) == 7:7
+                @test binarysearch([634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1], 144, lt = >,) == 4:4
+                @test binarysearch([377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1], 21, lt = >,) == 7:7
             end
             @testset "value not in array" begin
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 7, lt = >) == 4:3
@@ -116,10 +81,7 @@ if @isdefined(enable_bonus_tasks) && enable_bonus_tasks
             @test binarysearch([1, 3, 4, 6, 6, 9, 11], 6) == 4:5
             @test binarysearch([1, 1, 4, 6, 8, 9, 11], 1) == 1:2
             @test binarysearch([1, 3, 4, 6, 8, 11, 11], 11) == 6:7
-            @test binarysearch(
-                [1, 3, 5, 8, 13, 21, 34, 55, 144, 144, 144, 377, 634],
-                144,
-            ) == 9:11
+            @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 144, 144, 144, 377, 634],144,) == 9:11
             @test binarysearch([1, 3, 5, 21, 21, 21, 21, 21, 89, 144, 233, 377], 21) == 4:8
         end
     end
