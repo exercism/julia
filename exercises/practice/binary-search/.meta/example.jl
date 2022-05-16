9274e9618e1c60ae901e0181a1c5c6b5698ee753
Giovanni Puccetti <g.puccetti92@gmail.com>
2022-05-16 15:54:31 +0200

    Improve binary search (#508)
    * Add test for multiple matches and make bonus task runnable
    
    Add initial test for multiple matches and a line to uncomment
    allowing to run tests for the bonus tasks
    
    * use @isdefined
    
    * add match multiple cases to bonus tasks in description
    
    * add message showing that bonus tasks are enabled
    
    * add line at end of file
    
    * remove leftore variable
    
    * format
    
    * Remove unused description.md
    
    * Improve instructions
    
    * Enable bonus tests in example.jl so that they're tested by CI
    
    * Remove jargon
    
    "Dichotomic divide and conquer search algorithm"? Get outta here!
    
    * remove print statement and add separate flags for each bonus task
    
    * switch from single variables to named tuple to test bonus tasks
    
    * add end of file
    
    * undo format
    
    Co-authored-by: Colin Caine <cmcaine@gmail.com>

--- exercises/practice/binary-search/.meta/example.jl
+++ exercises/practice/binary-search/.meta/example.jl
@@ -1,2 +1,4 @@
 # the base function searchsorted does exactly what the exercise asks for
 const binarysearch = searchsorted
+
+tested_benus_tasks = (rev = true, by = true, lt = true, multiple_matches = true)
---
+++ exercises/practice/binary-search/binary-search.jl
@@ -0,0 +1,3 @@
+# Change any of the following NamedTuple values to true to 
+# enable testing different bonus tasks
+tested_benus_tasks = (rev = false, by = false, lt = false, multiple_matches = false)
