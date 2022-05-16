1a4b7958f9a7ce5c2e63427919ad0ef7fde1c559
gpucce <g.puccetti92@gmail.com>
2022-05-16 15:45:48 +0200

    undo format

--- exercises/practice/binary-search/runtests.jl
+++ exercises/practice/binary-search/runtests.jl
@@ -8,8 +8,7 @@ include("binary-search.jl")
         @test binarysearch([1, 3, 4, 6, 8, 9, 11], 6) == 4:4
         @test binarysearch([1, 3, 4, 6, 8, 9, 11], 1) == 1:1
         @test binarysearch([1, 3, 4, 6, 8, 9, 11], 11) == 7:7
-        @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144) ==
-              10:10
+        @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144) == 10:10
         @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 21) == 6:6
     end
     @testset "value not in array" begin
@@ -29,16 +28,8 @@ end
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 6, rev = true) == 4:4
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 1, rev = true) == 7:7
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 11, rev = true) == 1:1
-                @test binarysearch(
-                    [634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    144,
-                    rev = true,
-                ) == 4:4
-                @test binarysearch(
-                    [377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    21,
-                    rev = true,
-                ) == 7:7
+                @test binarysearch([634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1], 144, rev = true,) == 4:4
+                @test binarysearch([377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1], 21, rev = true,) == 7:7
             end
             @testset "value not in array" begin
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 7, rev = true) == 4:3
@@ -55,21 +46,9 @@ end
                 @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 6, by = round) == 4:4
                 @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 1, by = round) == 1:1
                 @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 11, by = round) == 7:7
-                @test binarysearch(
-                    [1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8],
-                    11,
-                    by = abs2 ∘ round,
-                ) == 7:7
-                @test binarysearch(
-                    [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634],
-                    144.4,
-                    by = round,
-                ) == 10:10
-                @test binarysearch(
-                    [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377],
-                    20.6,
-                    by = round,
-                ) == 6:6
+                @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 11, by = abs2 ∘ round,) == 7:7
+                @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144.4, by = round,) == 10:10
+                @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 20.6, by = round,) == 6:6
             end
             @testset "value not in array" begin
                 @test binarysearch([1.1, 2.9, 4.4, 5.5, 8.1, 9.0, 10.8], 7, by = round) == 5:4
@@ -88,16 +67,8 @@ end
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 6, lt = >) == 4:4
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 1, lt = >) == 7:7
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 11, lt = >) == 1:1
-                @test binarysearch(
-                    [634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    144,
-                    lt = >,
-                ) == 4:4
-                @test binarysearch(
-                    [377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1],
-                    21,
-                    lt = >,
-                ) == 7:7
+                @test binarysearch([634, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1], 144, lt = >,) == 4:4
+                @test binarysearch([377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 1], 21, lt = >,) == 7:7
             end
             @testset "value not in array" begin
                 @test binarysearch([11, 9, 8, 6, 4, 3, 1], 7, lt = >) == 4:3
@@ -113,10 +84,7 @@ end
             @test binarysearch([1, 3, 4, 6, 6, 9, 11], 6) == 4:5
             @test binarysearch([1, 1, 4, 6, 8, 9, 11], 1) == 1:2
             @test binarysearch([1, 3, 4, 6, 8, 11, 11], 11) == 6:7
-            @test binarysearch(
-                [1, 3, 5, 8, 13, 21, 34, 55, 144, 144, 144, 377, 634],
-                144,
-            ) == 9:11
+            @test binarysearch([1, 3, 5, 8, 13, 21, 34, 55, 144, 144, 144, 377, 634],144,) == 9:11
             @test binarysearch([1, 3, 5, 21, 21, 21, 21, 21, 89, 144, 233, 377], 21) == 4:8
         end
     end
