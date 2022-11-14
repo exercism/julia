13171d9a3e86676307ed59231b3e96553034e5be
Colin Caine <cmcaine@gmail.com>
2022-11-14 14:40:37 +0000

    Adapt test runner for concept.wip

--- runtestrunner.jl
+++ runtestrunner.jl
@@ -11,40 +11,29 @@ results_dir = mktempdir(; prefix="jl_test-runner_results_", cleanup=false)
 # In CI, set results directory as output
 set_output("results-path", results_dir)
 
-for exercise_type in ("concept", "practice")
-    for exercise in readdir(joinpath("exercises", exercise_type))
-        # Allow only testing specified exercises
-        if !isempty(ARGS) && !(exercise in ARGS)
-            continue
-        end
+include("eachexercise.jl")
 
-        exercise_path = joinpath("exercises", exercise_type, exercise)
-        isdir(exercise_path) || continue
+eachexercise(ARGS) do exercise, exercise_type, exercise_path, example_path
+    @info "[$(uppercase(exercise_type))] Testing $exercise"
 
-        # Concept exercises have an exemplar, practice exercises merely an example for CI purposes
-        example_or_exemplar = exercise_type == "concept" ? "exemplar" : "example"
+    # Determine file name of the solution
+    meta_cfg = JSON.parsefile(joinpath(exercise_path, ".meta", "config.json"))
+    solution_file = meta_cfg["files"]["solution"][1]
 
-        @info "[$(uppercase(exercise_type))] Testing $exercise"
-        
-        # Determine file name of the solution
-        meta_cfg = JSON.parsefile(joinpath(exercise_path, ".meta", "config.json"))
-        solution_file = meta_cfg["files"]["solution"][1]
+    # Copy solution to temporary directory
+    tmp = mktempdir(; prefix="jl_$(exercise)_")
+    cp(example_path, joinpath(tmp, solution_file))
+    cp(joinpath(exercise_path, "runtests.jl"), joinpath(tmp, "runtests.jl"))
 
-        # Copy solution to temporary directory
-        tmp = mktempdir(; prefix="jl_$(exercise)_")
-        cp(joinpath(exercise_path, ".meta", example_or_exemplar * ".jl"), joinpath(tmp, solution_file))
-        cp(joinpath(exercise_path, "runtests.jl"), joinpath(tmp, "runtests.jl"))
-        
-        # Run test-runner
-        run(`docker run
-                --network none
-                --read-only
-                --mount type=bind,src=$tmp,dst=/solution
-                --mount type=bind,src=$tmp,dst=/output
-                --mount type=tmpfs,dst=/tmp
-                exercism/julia-test-runner $exercise /solution /output`
-        )
+    # Run test-runner
+    run(`docker run
+            --network none
+            --read-only
+            --mount type=bind,src=$tmp,dst=/solution
+            --mount type=bind,src=$tmp,dst=/output
+            --mount type=tmpfs,dst=/tmp
+            exercism/julia-test-runner $exercise /solution /output`
+    )
 
-        cp(joinpath(tmp, "results.json"), joinpath(results_dir, "$(exercise_type)_$exercise.json"))
-    end
+    cp(joinpath(tmp, "results.json"), joinpath(results_dir, "$(exercise_type)_$exercise.json"))
 end
