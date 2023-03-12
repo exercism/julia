e8410fdcac93d014c000d42fbb50eb59a8c48a54
colinleach <colin.leach@comcast.net>
2023-03-12 10:36:53 -0700

    config.json manually edited

--- exercises/practice/sum-of-multiples/.meta/example.jl
+++ exercises/practice/sum-of-multiples/.meta/example.jl
@@ -1,5 +1,5 @@
 function sum_of_multiples(limit, multiples)
-    uniques = Set([0])
+    uniques = Set([0]) # avoid empty set, which would make sum() unhappy
     for num in [m for m in multiples if m > 0]
         mult = num
         while mult < limit
