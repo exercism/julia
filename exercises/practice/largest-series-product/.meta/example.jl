ef79be6e7cc48d245c8a8cc54cab911fae3ea9ba
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-14 14:44:30 +0100

    largest-series-product: add exercise

---
+++ exercises/practice/largest-series-product/.meta/example.jl
@@ -0,0 +1,16 @@
+function largest_product(s::AbstractString, span::Int)
+    0 <= span <= length(s) || throw(DomainError(span, "Span must be ≥ 0 and ≤ length(s)"))
+    (iszero(span) || isempty(s)) && return 1
+
+    digits = zeros(Int8, span)
+    largest = 0
+
+    for c in s
+        isdigit(c) || throw(DomainError(s, "String must only contain digits"))
+        popfirst!(digits)
+        push!(digits, c - '0')
+        largest = max(largest, prod(digits))
+    end
+
+    largest
+end
