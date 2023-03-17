474474d9166ae7651bb2c0332a15cdb54075fb7a
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-17 16:05:48 +0100

    Revert

--- exercises/practice/saddle-points/.meta/example.jl
+++ exercises/practice/saddle-points/.meta/example.jl
@@ -2,5 +2,7 @@ function saddlepoints(M)
     isempty(M) && return Tuple{Int,Int}[]
 
     cols_min = minimum(M, dims=1)
-    [Tuple(i) for i = CartesianIndices(M) if M[i] <= cols_min[i[2]] && M[i] >= maximum(view(M, i[1], :))]
+    rows_max = maximum(M, dims=2)
+
+    [Tuple(i) for i = CartesianIndices(M) if rows_max[i[1]] <= M[i] <= cols_min[i[2]]]
 end
--- exercises/practice/saddle-points/saddle-points.jl
+++ exercises/practice/saddle-points/saddle-points.jl
@@ -1,2 +1,3 @@
 function saddlepoints(M)
+
 end
