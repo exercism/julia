4d4adab68af184d4eeed5fa81938efb9866c8e07
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-17 08:12:55 +0100

    saddle-points: add exercise

---
+++ exercises/practice/saddle-points/.meta/example.jl
@@ -0,0 +1,8 @@
+function saddlepoints(matrix)
+    isempty(matrix) && return []
+
+    (cols_min,) = findmin(matrix, dims=1)
+    (rows_max,) = findmax(matrix, dims=2)
+
+    [Tuple(i) for i = CartesianIndices(matrix) if matrix[i] <= cols_min[i[2]] && matrix[i] >= rows_max[i[1]]]
+end
---
+++ exercises/practice/saddle-points/saddle-points.jl
@@ -0,0 +1,2 @@
+function saddlepoints(matrix)
+end
