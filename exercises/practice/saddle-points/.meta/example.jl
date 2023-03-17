feef3f805750b2b6b34d5801757898e85941f5e5
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-17 14:19:02 +0100

    Update exercises/practice/saddle-points/.meta/example.jl
    Co-authored-by: Colin Caine <cmcaine@gmail.com>

--- exercises/practice/saddle-points/.meta/example.jl
+++ exercises/practice/saddle-points/.meta/example.jl
@@ -2,7 +2,7 @@ function saddlepoints(matrix)
     isempty(matrix) && return Tuple{Int,Int}[]
 
     (cols_min,) = findmin(matrix, dims=1)
-    (rows_max,) = findmax(matrix, dims=2)
+    rows_max = maximum(matrix, dims=2)
 
     [Tuple(i) for i = CartesianIndices(matrix) if matrix[i] <= cols_min[i[2]] && matrix[i] >= rows_max[i[1]]]
 end
