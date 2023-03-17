4e2b5fd535f3512850ca979b78f6c00909845843
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-17 14:23:51 +0100

    Update example

--- exercises/practice/saddle-points/.meta/example.jl
+++ exercises/practice/saddle-points/.meta/example.jl
@@ -2,7 +2,5 @@ function saddlepoints(M)
     isempty(M) && return Tuple{Int,Int}[]
 
     cols_min = minimum(M, dims=1)
-    rows_max = maximum(M, dims=2)
-
-    [Tuple(i) for i = CartesianIndices(M) if M[i] <= cols_min[i[2]] && M[i] >= rows_max[i[1]]]
+    [Tuple(i) for i = CartesianIndices(M) if M[i] <= cols_min[i[2]] && M[i] >= maximum(view(M, i[1], :))]
 end
