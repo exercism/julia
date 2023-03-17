efed1ef2749b6502d6be7a1d59de68ff49716623
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-17 14:24:52 +0100

    Rename in tests

--- exercises/practice/saddle-points/runtests.jl
+++ exercises/practice/saddle-points/runtests.jl
@@ -3,46 +3,46 @@ using Test
 include("saddle-points.jl")
 
 @testset "Can identify single saddle point" begin
-    matrix = [9 8 7; 5 3 2; 6 6 7]
-    @test saddlepoints(matrix) == [(2, 1)]
+    M = [9 8 7; 5 3 2; 6 6 7]
+    @test saddlepoints(M) == [(2, 1)]
 end
 
 @testset "Can identify that empty matrix has no saddle points" begin
-    matrix = []
-    @test saddlepoints(matrix) == []
+    M = []
+    @test saddlepoints(M) == []
 end
 
 @testset "Can identify lack of saddle points when there are none" begin
-    matrix = [1 2 3; 3 1 2; 2 3 1]
-    @test saddlepoints(matrix) == []
+    M = [1 2 3; 3 1 2; 2 3 1]
+    @test saddlepoints(M) == []
 end
 
 @testset "Can identify multiple saddle points in a column" begin
-    matrix = [4 5 4; 3 5 5; 1 5 4]
-    @test saddlepoints(matrix) == [(1, 2), (2, 2), (3, 2)]
+    M = [4 5 4; 3 5 5; 1 5 4]
+    @test saddlepoints(M) == [(1, 2), (2, 2), (3, 2)]
 end
 
 @testset "Can identify multiple saddle points in a row" begin
-    matrix = [6 7 8; 5 5 5; 7 5 6]
-    @test saddlepoints(matrix) == [(2, 1), (2, 2), (2, 3)]
+    M = [6 7 8; 5 5 5; 7 5 6]
+    @test saddlepoints(M) == [(2, 1), (2, 2), (2, 3)]
 end
 
 @testset "Can identify saddle point in bottom right corner" begin
-    matrix = [8 7 9; 6 7 6; 3 2 5]
-    @test saddlepoints(matrix) == [(3, 3)]
+    M = [8 7 9; 6 7 6; 3 2 5]
+    @test saddlepoints(M) == [(3, 3)]
 end
 
 @testset "Can identify saddle points in a non square matrix" begin
-    matrix = [3 1 3; 3 2 4]
-    @test saddlepoints(matrix) == [(1, 1), (1, 3)]
+    M = [3 1 3; 3 2 4]
+    @test saddlepoints(M) == [(1, 1), (1, 3)]
 end
 
 @testset "Can identify that saddle points in a single column matrix are those with the minimum value" begin
-    matrix = reshape([2, 1, 4, 1], :, 1)
-    @test saddlepoints(matrix) == [(2, 1), (4, 1)]
+    M = reshape([2, 1, 4, 1], :, 1)
+    @test saddlepoints(M) == [(2, 1), (4, 1)]
 end
 
 @testset "Can identify that saddle points in a single row matrix are those with the maximum value" begin
-    matrix = [2 5 3 5]
-    @test saddlepoints(matrix) == [(1, 2), (1, 4)]
+    M = [2 5 3 5]
+    @test saddlepoints(M) == [(1, 2), (1, 4)]
 end
