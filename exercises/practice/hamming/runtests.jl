f63ea5b52fe88897a8280d58ba8c25acd2c545e4
yctai1994 <yctai.git@gmail.com>
2024-01-12 03:02:27 +0900

    add: verbose testing
    - killer-sudoku-helper
    - hamming
    - saddle-points
    - pangram (fix)
    - leap

--- exercises/practice/hamming/runtests.jl
+++ exercises/practice/hamming/runtests.jl
@@ -2,43 +2,45 @@ using Test
 
 include("hamming.jl")
 
-@testset "identical strands" begin
-    @test distance("A", "A") == 0
-    @test distance("GGACTGA", "GGACTGA") == 0
-end
+@testset verbose = true "Hamming" begin
+    @testset "identical strands" begin
+        @test distance("A", "A") == 0
+        @test distance("GGACTGA", "GGACTGA") == 0
+    end
 
-@testset "complete distance" begin
-    @test distance("A", "G") == 1
-    @test distance("AG", "CT") == 2
-end
+    @testset "complete distance" begin
+        @test distance("A", "G") == 1
+        @test distance("AG", "CT") == 2
+    end
 
-@testset "small distance" begin
-    @test distance("AT", "CT") == 1
-    @test distance("GGACG", "GGTCG") == 1
-    @test distance("ACCAGGG", "ACTATGG") == 2
-end
+    @testset "small distance" begin
+        @test distance("AT", "CT") == 1
+        @test distance("GGACG", "GGTCG") == 1
+        @test distance("ACCAGGG", "ACTATGG") == 2
+    end
 
-@testset "non-unique characters" begin
-    @test distance("AGA", "AGG") == 1
-    @test distance("AGG", "AGA") == 1
-end
+    @testset "non-unique characters" begin
+        @test distance("AGA", "AGG") == 1
+        @test distance("AGG", "AGA") == 1
+    end
 
-@testset "same nucleotides in different positions" begin
-    @test distance("TAG", "GAT") == 2
-end
+    @testset "same nucleotides in different positions" begin
+        @test distance("TAG", "GAT") == 2
+    end
 
-@testset "large distance" begin
-    @test distance("GATACA", "GCATAA") == 4
-    @test distance("GGACGGATTCTG", "AGGACGGATTCT") == 9
-end
+    @testset "large distance" begin
+        @test distance("GATACA", "GCATAA") == 4
+        @test distance("GGACGGATTCTG", "AGGACGGATTCT") == 9
+    end
 
-@testset "empty strands" begin
-    @test distance("", "") == 0
-end
+    @testset "empty strands" begin
+        @test distance("", "") == 0
+    end
 
-@testset "different strand lengths" begin
-    @test_throws ArgumentError distance("AATG", "AAA")
-    @test_throws ArgumentError distance("ATA", "AGTG")
-    @test_throws ArgumentError distance("", "G")
-    @test_throws ArgumentError distance("G", "")
+    @testset "different strand lengths" begin
+        @test_throws ArgumentError distance("AATG", "AAA")
+        @test_throws ArgumentError distance("ATA", "AGTG")
+        @test_throws ArgumentError distance("", "G")
+        @test_throws ArgumentError distance("G", "")
+    end
 end
