a4bc759940c1c92390b6428b370db595c9baac28
depial <91621102+depial@users.noreply.github.com>
2024-01-31 12:22:16 -0500

    Update runtests.jl
    Wrapped the test set in a function to be called at the end of the file so the helper functions can be put at the bottom of the file.

--- exercises/practice/perceptron/runtests.jl
+++ exercises/practice/perceptron/runtests.jl
@@ -1,8 +1,49 @@
 using Test, Random
-
 include("perceptron.jl")
 
-Random.seed!(42) # set seed for deterministic test set
+function runtestset()
+    
+    @testset "Low population" begin
+        @testset "Initial set" begin
+            points = [[1, 2], [3, 4], [-1, -2], [-3, -4], [2, 1], [1, 1]]
+            labels = [1, 1, -1, -1, 1, 1]
+            reference = [1, 2, 1]
+            hyperplane = perceptron(points, labels)
+            @test dotest(points, labels, hyperplane, reference)
+        end
+        @testset "Initial set w/ opposite labels" begin
+            points = [[1, 2], [3, 4], [-1, -2], [-3, -4], [2, 1], [1, 1]]
+            labels = [-1, -1, 1, 1, -1, -1]
+            reference = [-1, -2, -1]
+            hyperplane = perceptron(points, labels)
+            @test dotest(points, labels, hyperplane, reference)
+        end
+        @testset "Hyperplane cannot pass through origin" begin
+            points = [[1, 2], [3, 4], [-1, -2], [-3, -4], [2, 1], [-1, -1]]
+            labels = [1, 1, -1, -1, 1, 1]
+            reference = [-1, 3, 3]
+            hyperplane = perceptron(points, labels)
+            @test dotest(points, labels, hyperplane, reference)
+        end
+        @testset "Hyperplane nearly parallel with y-axis" begin
+            points = [[0, 50], [0, -50], [-2, 0], [1, 50], [1, -50], [2, 0]]
+            labels = [-1, -1, -1, 1, 1, 1]
+            reference = [2, 0, -1]
+            hyperplane = perceptron(points, labels)
+            @test dotest(points, labels, hyperplane, reference)
+        end
+    end
+    
+    @testset "Increasing Populations" begin
+        for n in 10:50
+            points, labels, reference = population(n, 25)
+            hyperplane = perceptron(points, labels)
+            @test dotest(points, labels, hyperplane, reference)
+        end
+    end
+    
+end
+
 
 function population(n, bound)
     # Builds a population of n points with labels {1, -1} in area bound x bound around a reference hyperplane
@@ -41,41 +82,5 @@ function dotest(points, labels, hyperplane, reference)
     end
 end
 
-@testset "Low population" begin
-    @testset "Initial set" begin
-        points = [[1, 2], [3, 4], [-1, -2], [-3, -4], [2, 1], [1, 1]]
-        labels = [1, 1, -1, -1, 1, 1]
-        reference = [1, 2, 1]
-        hyperplane = perceptron(points, labels)
-        @test dotest(points, labels, hyperplane, reference)
-    end
-    @testset "Initial set w/ opposite labels" begin
-        points = [[1, 2], [3, 4], [-1, -2], [-3, -4], [2, 1], [1, 1]]
-        labels = [-1, -1, 1, 1, -1, -1]
-        reference = [-1, -2, -1]
-        hyperplane = perceptron(points, labels)
-        @test dotest(points, labels, hyperplane, reference)
-    end
-    @testset "Hyperplane cannot pass through origin" begin
-        points = [[1, 2], [3, 4], [-1, -2], [-3, -4], [2, 1], [-1, -1]]
-        labels = [1, 1, -1, -1, 1, 1]
-        reference = [-1, 3, 3]
-        hyperplane = perceptron(points, labels)
-        @test dotest(points, labels, hyperplane, reference)
-    end
-    @testset "Hyperplane nearly parallel with y-axis" begin
-        points = [[0, 50], [0, -50], [-2, 0], [1, 50], [1, -50], [2, 0]]
-        labels = [-1, -1, -1, 1, 1, 1]
-        reference = [2, 0, -1]
-        hyperplane = perceptron(points, labels)
-        @test dotest(points, labels, hyperplane, reference)
-    end
-end
-
-@testset "Increasing Populations" begin
-    for n in 10:50
-        points, labels, reference = population(n, 25)
-        hyperplane = perceptron(points, labels)
-        @test dotest(points, labels, hyperplane, reference)
-    end
-end
+Random.seed!(42) # set seed for deterministic test set
+runtestset()
