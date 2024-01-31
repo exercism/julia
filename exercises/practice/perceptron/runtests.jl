053cb6377e4741c1ed3400f171f3781e54d3808e
depial <91621102+depial@users.noreply.github.com>
2024-01-31 09:05:23 -0500

    Update runtests.jl
    Added helper functions from `testtools.jl` to bottom of file so it doesn't have to be included. Since the `dotest` function does not show a working solution (only how to check for one), I think a spoiler warning is unnecessary, and could just end up attracting more attention than without.

--- exercises/practice/perceptron/runtests.jl
+++ exercises/practice/perceptron/runtests.jl
@@ -1,7 +1,6 @@
-using Test
+using Test, Random
 
 include("perceptron.jl")
-include("testtools.jl")
 
 @testset "Low population" begin
     @testset "Initial set" begin
@@ -39,5 +38,46 @@ end
         points, labels, reference = population(n, 25)
         hyperplane = perceptron(points, labels)
         @test dotest(points, labels, hyperplane, reference)
+    end
+end
+
+
+
+Random.seed!(42) # set seed for deterministic test set
+
+function population(n, bound)
+    # Builds a population of n points with labels {1, -1} in area bound x bound around a reference hyperplane
+    # Returns linearly separable points, labels and reference hyperplane
+
+    vertical = !iszero(n % 10) #every tenth test has vertical reference hyperplane
+    x, y, b = rand(-bound:bound), rand(-bound:bound)*vertical, rand(-bound÷2:bound÷2)
+    y_intercept = -b ÷ (iszero(y) ? 1 : y)
+    points, labels, hyperplane = [], [], [b, x, y]
+    while n > 0
+        # points are centered on y-intercept, but not x-intercept so distributions can be lopsided
+        point = [rand(-bound:bound), y_intercept + rand(-bound:bound)]
+        label = point' * [x, y] + b
+        if !iszero(label)
+            push!(points, point)
+            push!(labels, sign(label))
+            n -= 1
+        end
+    end
+
+    points, labels, hyperplane
+end 
+
+function dotest(points, labels, hyperplane, reference)
+    points = vcat.(1, points)
+    test = reduce(hcat, points)' * hyperplane .* labels
+    if all(>(0), test)
+        println("Reference hyperplane = $reference\nYour hyperplane = $hyperplane\nSeparated! And the normal points towards the positively labeled side\n")
+        return true
+    elseif all(<(0), test)
+        println("Reference hyperplane = $reference\nYour hyperplane = $hyperplane\nSeparated! But the normal points towards the negatively labeled side\n")
+        return true
+    else
+        println("Reference hyperplane = $reference\nYour hyperplane = $hyperplane\nThe sides are not properly separated...\n")
+        return false
     end
 end
