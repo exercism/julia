87024231734c8279de527d248a76157aa351da26
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-02-04 22:14:03 -0800

    Add resistor-color exercise

---
+++ exercises/practice/resistor-color/.meta/example.jl
@@ -0,0 +1,20 @@
+COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+]
+
+function colorcode(color)
+    findfirst(==(color), COLORS) - 1
+end
+
+function colors()
+    COLORS
+end
