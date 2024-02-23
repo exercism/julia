9c8193d8b34cd3691e49066b1f65b26a723d5acc
depial <91621102+depial@users.noreply.github.com>
2024-02-22 19:49:46 -0500

    Update runtests.jl
    Cleaned up obsolete functionality, and tried to include reporting of students' decision boundaries in testset titles, for easier debugging

--- exercises/practice/perceptron/runtests.jl
+++ exercises/practice/perceptron/runtests.jl
@@ -1,54 +1,65 @@
 using Test, Random
 include("perceptron.jl")
 
+decisionboundary = perceptron([[-1,-1], [1, 0], [0, 1]], [-1, 1, 1])
+
 @testset "Boundary is a vector of three weights" begin
-    decisionboundary = perceptron([[-1,-1], [1, 0], [0, 1]], [-1, 1, 1])
     @test length(decisionboundary) == 3
 end
 
 @testset "Weights are Real numbers" begin
-    decisionboundary = perceptron([[-1,-1], [1, 0], [0, 1]], [-1, 1, 1])
     @test eltype(decisionboundary) <: Real
 end
 
 function runtestset()
     
     @testset "Low populations" begin
-        @testset "Initial population" begin
-            points = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-            labels = [-1, -1, 1, 1]
-            reference = [0, 1, 1]
-            decisionboundary = perceptron(points, labels)
-            @test isvalidboundary(points, labels, decisionboundary, reference)
+
+        # Initial population
+        points = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        labels = [-1, -1, 1, 1]
+        decisionboundary = perceptron(points, labels)
+        reference = [0, 1, 1] #A valid decision boundary need not match the reference
+        @testset "Initial population - Your decision boundary: $decisionboundary" begin
+            @test isvalidboundary(points, labels, decisionboundary)
         end
-        @testset "Initial population w/ opposite labels" begin
-            points = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-            labels = [1, 1, -1, -1]
-            reference = [0, -1, -1]
-            decisionboundary = perceptron(points, labels)
-            @test isvalidboundary(points, labels, decisionboundary, reference)
+
+        #Initial population w/ opposite labels
+        points = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        labels = [1, 1, -1, -1]
+        decisionboundary = perceptron(points, labels)
+        reference = [0, -1, -1] #A valid decision boundary need not match the reference
+        @testset "Initial population w/ opposite labels - Your decision boundary: $decisionboundary" begin
+            @test isvalidboundary(points, labels, decisionboundary)
         end
-        @testset "Decision boundary cannot pass through origin" begin
-            points = [[1, 0], [0, 1], [2, 1], [1, 2]]
-            labels = [-1, -1, 1, 1]
-            reference = [-2, 1, 1]
-            decisionboundary = perceptron(points, labels)
-            @test isvalidboundary(points, labels, decisionboundary, reference)
+
+        # Decision boundary cannot pass through origin
+        points = [[1, 0], [0, 1], [2, 1], [1, 2]]
+        labels = [-1, -1, 1, 1]
+        decisionboundary = perceptron(points, labels)
+        reference = [-2, 1, 1] #A valid decision boundary need not match the reference
+        @testset "Decision boundary cannot pass through origin - Your decision boundary: $decisionboundary" begin
+            @test isvalidboundary(points, labels, decisionboundary)
         end
-        @testset "Decision boundary nearly parallel with y-axis" begin
-            points = [[0, 50], [0, -50], [1, 50], [1, -50]]
-            labels = [-1, -1, 1, 1]
-            reference = [-1, 2, 0]
-            decisionboundary = perceptron(points, labels)
-            @test isvalidboundary(points, labels, decisionboundary, reference)
+
+        #Decision boundary nearly parallel with y-axis
+        points = [[0, 50], [0, -50], [1, 50], [1, -50]]
+        labels = [-1, -1, 1, 1]
+        decisionboundary = perceptron(points, labels)
+        reference = [-1, 2, 0] #A valid decision boundary need not match the reference
+        @testset "Decision boundary nearly parallel with y-axis - Your decision boundary: $decisionboundary" begin
+            @test isvalidboundary(points, labels, decisionboundary)
         end
+        
     end
     
     @testset "Increasing Populations" begin
         for n in 10:50
-            points, labels, reference = population(n, 25)
+            points, labels = population(n, 25)
             decisionboundary = perceptron(points, labels)
-            @test isvalidboundary(points, labels, decisionboundary, reference)
+            @testset "Population: $n points - Your decision boundary: $decisionboundary" begin
+                @test isvalidboundary(points, labels, decisionboundary)
+            end
         end
     end
     
@@ -74,22 +85,12 @@ function population(n, bound)
         end
     end
 
-    points, labels, hyperplane
+    points, labels
 end 
 
-function isvalidboundary(points, labels, hyperplane, reference)
+function isvalidboundary(points, labels, decisionboundary)
     points = vcat.(1, points)
-    test = reduce(hcat, points)' * hyperplane .* labels
-    if all(>(0), test)
-        println("Reference hyperplane = $reference\nYour hyperplane = $hyperplane\nSeparated! And the normal points towards the positively labeled class\n")
-        return true
-    elseif all(<(0), test)
-        println("Reference hyperplane = $reference\nYour hyperplane = $hyperplane\nSeparated! But the normal points towards the negatively labeled class\n")
-        return false
-    else
-        println("Reference hyperplane = $reference\nYour hyperplane = $hyperplane\nThe classes are not properly separated\n")
-        return false
-    end
+    all(>(0), reduce(hcat, points)' * decisionboundary .* labels)
 end
 
 Random.seed!(42) # set seed for deterministic test set
