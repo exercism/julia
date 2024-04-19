1c44c5380e7bcaba069593289f7892d79a1d5dab
depial <91621102+depial@users.noreply.github.com>
2024-04-19 11:22:38 -0400

    Update runtests.jl
    Added description to top level testset

--- exercises/practice/connect/runtests.jl
+++ exercises/practice/connect/runtests.jl
@@ -2,7 +2,7 @@ using Test
 
 include("connect.jl")
 
-@testset verbose = true "" begin
+@testset verbose = true "tests" begin
     @testset "an empty board has no winner" begin
         board = [
             ". . . . .",
