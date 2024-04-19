8335d87f201feef6be73c2324db6c3bb2cc60cfb
Or Golan <golanor@users.noreply.github.com>
2024-04-19 13:46:11 +0300

    use argument error instead of a new user-defined error
    Co-authored-by: Colin Caine <cmcaine@gmail.com>

--- exercises/practice/protein-translation/runtests.jl
+++ exercises/practice/protein-translation/runtests.jl
@@ -67,11 +67,11 @@ include("protein-translation.jl")
     end
 
     @testset "Non existent codon causes translation exception" begin
-        @test_throws TranslationError rna_to_amino_acids("AAA")
+        @test_throws ArgumentError rna_to_amino_acids("AAA")
     end
 
     @testset "Incomplete codon causes translation exception" begin
-        @test_throws TranslationError rna_to_amino_acids("UGUU")
+        @test_throws ArgumentError rna_to_amino_acids("UGUU")
     end
 
     @testset "Incomplete RNA sequence can translate if given a stop codon" begin
