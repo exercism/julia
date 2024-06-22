21f8c8aed38c23bf3d7f658a217e7f6f4f425513
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-06-21 20:56:30 -0700

    Add pig-latin

---
+++ exercises/practice/pig-latin/.meta/example.jl
@@ -0,0 +1,18 @@
+function translate(phrase)
+    words = eachsplit(phrase, " ")
+    fragments = map(translateFragment, words)
+
+    join(fragments, " ")
+end
+
+vowelSound = r"^([aeiou]|xr|yt)"
+consonantSound = r"^([^aeiou]+(?=y)|[^aeiou]?qu|[^aeiou]+)([a-z]+)"
+
+function translateFragment(fragment)
+    translated = fragment
+    if !occursin(vowelSound, fragment)
+        translated = replace(fragment, consonantSound => s"\g<2>\g<1>")
+    end
+
+    translated * "ay"
+end
