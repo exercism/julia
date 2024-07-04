32da8a53a2cd038976eb833f3af70a7592ca34cc
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-07-03 22:28:52 -0700

    Update example.jl

--- exercises/practice/protein-translation/.meta/example.jl
+++ exercises/practice/protein-translation/.meta/example.jl
@@ -1,40 +1,39 @@
 function proteins(strand)
-    # chunk strands into groups of up to three
-        codons = chunked(strand)
-        proteins = []
-        for codon in codons
-            if !haskey(codon_to_protein, codon)
-                throw(DomainError(codon, "Invalid codon"))
-            end
-    
-            protein = codon_to_protein[codon]
-            protein == "STOP" && return proteins
-        
-            push!(proteins, protein)
+    codons = chunked(strand)
+    results = []
+    for codon in codons
+        if !haskey(codon_to_protein, codon)
+            throw(DomainError(codon, "Invalid codon"))
         end
-        proteins
-    end
+
+        protein = codon_to_protein[codon]
+        protein == "STOP" && return results
     
-    function chunked(strand)
-        [strand[i:min(i + 3 - 1, end)] for i in 1:3:length(strand)]
+        push!(results, protein)
     end
-    
-    codon_to_protein = Dict(
-        "AUG" => "Methionine",
-        "UUU" => "Phenylalanine",
-        "UUC" => "Phenylalanine",
-        "UUA" => "Leucine",
-        "UUG" => "Leucine",
-        "UCU" => "Serine",
-        "UCC" => "Serine",
-        "UCA" => "Serine",
-        "UCG" => "Serine",
-        "UAU" => "Tyrosine",
-        "UAC" => "Tyrosine",
-        "UGU" => "Cysteine",
-        "UGC" => "Cysteine",
-        "UGG" => "Tryptophan",
-        "UAA" => "STOP",
-        "UAG" => "STOP",
-        "UGA" => "STOP"
-    )
+    results
+end
+
+function chunked(strand)
+    [strand[i:min(i + 3 - 1, end)] for i in 1:3:length(strand)]
+end
+
+codon_to_protein = Dict(
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UGG" => "Tryptophan",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+)
