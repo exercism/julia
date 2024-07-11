1439c468b3a1f7bc7eb923b4f10dab6aee3a6118
depial <91621102+depial@users.noreply.github.com>
2024-07-11 15:31:13 -0400

    change testing to true/false

--- exercises/practice/dominoes/runtests.jl
+++ exercises/practice/dominoes/runtests.jl
@@ -3,80 +3,55 @@ using Test
 include("dominoes.jl")
 
 @testset verbose = true "tests" begin
-    function runtestset()
-        @testset "empty input = empty output" begin
-            @test dominoes([]) == []
-        end
-
-        @testset "singleton input = singleton output" begin
-            chain = dominoes([1, 1])
-            @test isvalidchain(chain, true)
-        end
-
-        @testset "singleton that can't be chained" begin
-            chain = dominoes([1, 2])
-            @test isvalidchain(chain, false)
-        end
-
-        @testset "three elements" begin
-            chain = dominoes([[1, 2], [3, 1], [2, 3]])
-            @test isvalidchain(chain, true)
-        end
-
-        @testset "can reverse dominoes" begin
-            chain = dominoes([[1, 2], [1, 3], [2, 3]])
-            @test isvalidchain(chain, true)
-        end
-
-        @testset "can't be chained" begin
-            chain = dominoes([[1, 2], [4, 1], [2, 3]])
-            @test isvalidchain(chain, false)
-        end
-
-        @testset "disconnected - simple" begin
-            chain = dominoes([[1, 1], [2, 2]])
-            @test isvalidchain(chain, false)
-        end
-
-        @testset "disconnected - double loop" begin
-            chain = dominoes([[1, 2], [2, 1], [3, 4], [4, 3]])
-            @test isvalidchain(chain, false)
-        end
-
-        @testset "disconnected - single isolated" begin
-            chain = dominoes([[1, 2], [2, 3], [3, 1], [4, 4]])
-            @test isvalidchain(chain, false)
-        end
-
-        @testset "need backtrack" begin
-            chain = dominoes([[1, 2], [2, 3], [3, 1], [2, 4], [2, 4]])
-            @test isvalidchain(chain, true)
-        end
-
-        @testset "separate loops" begin
-            chain = dominoes([[1, 2], [2, 3], [3, 1], [1, 1], [2, 2], [3, 3]])
-            @test isvalidchain(chain, true)
-        end
-
-        @testset "nine elements" begin
-            chain = dominoes([[1, 2], [5, 3], [3, 1], [1, 2], [2, 4], [1, 6], [2, 3], [3, 4], [5, 6]])
-            @test isvalidchain(chain, true)
-        end
-
-        @testset "separate three-domino loops" begin
-            chain = dominoes([[1, 2], [2, 3], [3, 1], [4, 5], [5, 6], [6, 4]])
-            @test isvalidchain(chain, false)
-        end
+    @testset "empty input = empty output" begin
+        @test dominoes([]) == true
     end
-
-    function isvalidchain(chain, valid)
-        !valid && return isa(chain, Bool) ? !chain : false
-        first(first(chain)) != last(last(chain)) && return false
-        for i in 1:length(chain)-1
-            last(chain[i]) != first(chain[i+1]) && return false
-        end
-        true
+    
+    @testset "singleton input = singleton output" begin
+        @test dominoes([1, 1]) == true
     end
-
-    runtestset()
+    
+    @testset "singleton that can't be chained" begin
+        @test dominoes([1, 2]) == false
+    end
+    
+    @testset "three elements" begin
+        @test dominoes([[1, 2], [3, 1], [2, 3]]) == true
+    end
+    
+    @testset "can reverse dominoes" begin 
+        @test dominoes([[1, 2], [1, 3], [2, 3]]) == true
+    end
+    
+    @testset "can't be chained" begin
+        @test dominoes([[1, 2], [4, 1], [2, 3]]) == false
+    end
+    
+    @testset "disconnected - simple" begin
+        @test dominoes([[1, 1], [2, 2]]) == false
+    end
+    
+    @testset "disconnected - double loop" begin
+        @test dominoes([[1, 2], [2, 1], [3, 4], [4, 3]]) == false
+    end
+    
+    @testset "disconnected - single isolated" begin
+        @test dominoes([[1, 2], [2, 3], [3, 1], [4, 4]]) == false
+    end
+    
+    @testset "need backtrack" begin
+        @test dominoes([[1, 2], [2, 3], [3, 1], [2, 4], [2, 4]]) == true
+    end
+    
+    @testset "separate loops" begin
+        @test dominoes([[1, 2], [2, 3], [3, 1], [1, 1], [2, 2], [3, 3]]) == true
+    end
+    
+    @testset "nine elements" begin
+        @test dominoes([[1, 2], [5, 3], [3, 1], [1, 2], [2, 4], [1, 6], [2, 3], [3, 4], [5, 6]]) == true
+    end
+    
+    @testset "separate three-domino loops" begin
+        @test dominoes([[1, 2], [2, 3], [3, 1], [4, 5], [5, 6], [6, 4]]) == false
+    end
 end
