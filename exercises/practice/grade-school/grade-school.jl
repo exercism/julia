881a57209b73d5d8fd0bab7f22033bcb0d307958
colinleach <colin.leach@comcast.net>
2024-09-01 15:07:16 -0700

    add practice exercise grade-school

---
+++ exercises/practice/grade-school/grade-school.jl
@@ -0,0 +1,19 @@
+# The new_school() function must return something that can be used in
+# all the other functions.
+# Programmers are free to choose a suitable implementation.
+
+function new_school()
+
+end
+
+function add!(students, school)
+
+end
+
+function grade(num, school)
+    
+end
+
+function roster(school)
+
+end
