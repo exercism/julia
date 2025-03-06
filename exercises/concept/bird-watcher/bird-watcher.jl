2fddc90b0decd8e67fddd3b03029c38493c37175
Colin Leach <colinleach@pm.me>
2025-03-06 13:21:01 -0700

    `bird-watcher` concept exercise (#838)
    * `bird-watcher` concept exercise
    
    * Update introduction.md
    
    Clear up two typos
    
    * Update instructions.md
    
    added demo for `average_per_day()`
    
    * Update instructions.md
    
    ---------
    
    Co-authored-by: Colin Leach <colin.ex@pm.me>
    Co-authored-by: depial <91621102+depial@users.noreply.github.com>

---
+++ exercises/concept/bird-watcher/bird-watcher.jl
@@ -0,0 +1,23 @@
+function today(birds_per_day)
+    
+end
+
+function increment_todays_count(birds_per_day)
+    
+end
+
+function has_day_without_birds(birds_per_day)
+    
+end
+
+function count_for_first_days(birds_per_day, num_days)
+    
+end
+
+function busy_days(birds_per_day) 
+    
+end
+
+function average_per_day(week1, week2)
+    
+end
