bde00da8605519533b6420441b987f1a0c5932d0
Colin Leach <colinleach@pm.me>
2025-06-11 10:47:52 -0700

    Update runtests.jl
    replaced with @depial version

--- exercises/concept/factory-sensors/runtests.jl
+++ exercises/concept/factory-sensors/runtests.jl
@@ -3,49 +3,50 @@ using Test
 include("factory-sensors.jl")
 
 @testset verbose = true "tests" begin
-
     @testset "1. Monitor the humidity level of the room" begin
         @testset "Passing" begin
-            @test humidity_level_ok(53)
+            @test_logs (:info, "humidity level check passed: 53%") humiditycheck(53)
         end
 
         @testset "Failing" begin
-            @test_throws ErrorException humidity_level_ok(80)
-            @test_throws "80" humidity_level_ok(80)
+            @test_throws ErrorException humiditycheck(80)
+            @test_throws "80" humiditycheck(80)
         end       
     end
 
     @testset "2. Check for overheating" begin
         @testset "Passing" begin
-            @test temperature_ok(200)
+            @test_logs (:info, "temperature check passed: 200 °C") temperaturecheck(200)
         end
 
         @testset "Failing" begin
-            @test_throws ArgumentError temperature_ok(nothing)
-            @test_throws DomainError temperature_ok(501)
-            @test_throws "501" temperature_ok(501)
+            @test_throws ArgumentError temperaturecheck(nothing)
+            @test_throws DomainError temperaturecheck(501)
+            @test_throws "501" temperaturecheck(501)
         end
     end
 
     @testset "3. Monitor the machine" begin
         @testset "Passing" begin
-            @test_logs (:info, "Machine is running smoothly") monitor_the_machine(53, 200)
+            @test_logs (:info, "humidity level check passed: 53%") (:info, "temperature check passed: 200 °C") machinemonitor(53, 200)
         end
 
         @testset "Failing" begin
-            @test_throws ErrorException humidity_level_ok(80)
-            @test_throws "80" humidity_level_ok(80)
-
-            @test_throws ArgumentError temperature_ok(nothing)
-            @test_throws DomainError temperature_ok(501)
-            @test_throws "501" temperature_ok(501)
+            @test_logs (:error, "humidity level check failed: 80%") (:info, "temperature check passed: 220 °C") begin
+                @test_throws MachineError machinemonitor(80, 220) 
+            end
+            @test_logs (:info, "humidity level check passed: 52%") (:warn, "sensor is broken") begin
+                @test_throws MachineError machinemonitor(52, nothing)
+            end       
+            @test_logs (:info, "humidity level check passed: 21%") (:error, "overheating detected: 540 °C") begin
+                @test_throws MachineError machinemonitor(21, 540) 
+            end
+            @test_logs (:error, "humidity level check failed: 100%") (:warn, "sensor is broken") begin
+                @test_throws MachineError machinemonitor(100, nothing) 
+            end
+            @test_logs (:error, "humidity level check failed: 93%") (:error, "overheating detected: 521 °C") begin
+                @test_throws MachineError machinemonitor(93, 521)
+            end
         end       
-
-        # I can't get this working:
-        
-        # @testset "Failing humidity" begin
-        #     @test_logs (:error, "Humidity level check failed: 100%") monitor_the_machine(100, 200)
-        # end
-
     end
-end
+    
