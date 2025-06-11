16a38e76f70fcaaa2e60088a7edea3c11238d124
Colin Leach <colinleach@pm.me>
2025-06-11 10:46:57 -0700

    Update exemplar.jl
    replaced with @depial version

--- exercises/concept/factory-sensors/.meta/exemplar.jl
+++ exercises/concept/factory-sensors/.meta/exemplar.jl
@@ -1,43 +1,29 @@
-humidity_level_ok(pct_humidity) = 
-    pct_humidity <= 70 ? true : error("Humidity level too high: $pct_humidity%")
-
-function temperature_ok(temperature)
-    isnothing(temperature) && throw(ArgumentError("Sensor broken"))
-    temperature > 500 && throw(DomainError(temperature, "Overheating!"))
-    true
+function humiditycheck(pct_humidity)
+    pct_humidity > 70 && error("humidity level too high: $pct_humidity%")
+    @info "humidity level check passed: $pct_humidity%"
 end
 
-function monitor_the_machine(pct_humidity, temperature)
-    humidity_level_ok(pct_humidity)
-    temperature_ok(temperature)
-    @info "Machine is running smoothly"
+function temperaturecheck(temperature)
+    isnothing(temperature) && throw(ArgumentError("sensor is broken"))
+    temperature > 500 && throw(DomainError(temperature))
+    @info "temperature check passed: $temperature °C"
 end
 
+struct MachineError <: Exception end
 
-
-# This is what I failed to write working tests for
-function old_monitor_the_machine(pct_humidity, temperature)
+function machinemonitor(pct_humidity, temperature)
+    errors = 0
     try
-        humidity_level_ok(pct_humidity) 
-    catch e
-        @error "Humidity level check failed: $pct_humidity%"
-        rethrow()
+        humiditycheck(pct_humidity)
+    catch humidity_error
+        errors += 1
+        @error "humidity level check failed: $pct_humidity%"
     end
-
     try
-        temperature_ok(temperature)
-    catch e
-        if e isa ArgumentError
-            @error "Sensor is broken!"
-        elseif e isa DomainError
-            if temperature > 550
-                @error "Dangerous overheating detected: $temperature °C" 
-            else
-                @warn "Overheating detected: $temperature °C"
-            end
-        end
-        rethrow()
+        temperaturecheck(temperature)
+    catch temp_error
+        errors += 1
+        temp_error isa ArgumentError ? (@warn "sensor is broken") : (@error "overheating detected: $temperature °C")    
     end
-    
-    @info "Machine is running smoothly"
+    iszero(errors) || throw(MachineError())
 end
