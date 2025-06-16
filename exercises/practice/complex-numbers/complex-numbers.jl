30d4d052a21d9c0621a66da15affb1c6aa1f86c1
depial <91621102+depial@users.noreply.github.com>
2025-06-16 15:05:50 -0400

    update tests for complex-numbers (#962)
    [no important files changed]

---
+++ exercises/practice/complex-numbers/complex-numbers.jl
@@ -0,0 +1,5 @@
+# Uncomment the following line to enable bonus tests involving arithmetic between real numbers and complex numbers.
+# enable_realcomplex_tests = true
+
+# Uncomment the following line to enable bonus tests for syntax sugar.
+# enable_syntaxsugar_tests = true
