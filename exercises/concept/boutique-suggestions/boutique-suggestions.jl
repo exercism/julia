bd956c56fd2887266122c3c909563209c84df887
depial <91621102+depial@users.noreply.github.com>
2025-08-19 18:49:03 -0400

    add concept exercise botique-suggestions

---
+++ exercises/concept/boutique-suggestions/boutique-suggestions.jl
@@ -0,0 +1,15 @@
+function clothingitem(categories, qualities)
+
+end
+
+function get_combinations(tops, bottoms)
+
+end
+
+function get_prices(combos)
+    
+end
+
+function filter_clashing(combos)
+
+end
---
+++ exercises/concept/boutique-suggestions/testtools.jl
@@ -0,0 +1,1 @@
+testitem = Dict ∘ zip
