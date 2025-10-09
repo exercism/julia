5286b9efebe19fd06f5444dbed09a7056126bd44
Colin Leach <colin.ex@pm.me>
2025-10-09 09:27:22 -0700

    depial version of example.jl

--- exercises/practice/state-of-tic-tac-toe/.meta/example.jl
+++ exercises/practice/state-of-tic-tac-toe/.meta/example.jl
@@ -1,53 +1,63 @@
-# Both solutions adapted from 2020 versions by Sascha Mann and Jeremy Walker
+
+function gamestate(board)
+    M = reshape([col == ' ' ? 0 : (-1)^(col == 'O') for row in board for col in row], (3,3))
+    mini, maxi = extrema([sum(M, dims=1)..., sum(M, dims=2)..., M[1,1]+M[2,2]+M[3,3], M[1,3]+M[2,2]+M[3,1]])
+    (sum(M) ∉ (0, 1) || maxi == 3 && mini == -3) && error("Invalid board")
+    (maxi == 3 || mini == -3) ? "win" : 0 ∈ M ? "ongoing" : "draw"
+end
+
+
+
+# Both solutions below are adapted from 2020 versions by Sascha Mann and Jeremy Walker
 # Significant changes were needed to pass the current tests
 
 # Version 1  (kind of elegant, for people who like linear algebra)
 # https://github.com/exercism/research_experiment_1/blob/julia-dev/exercises/julia-1-a/.meta/example2.jl
 
-using LinearAlgebra
+# using LinearAlgebra
 
-const x = 1
-const o = -1
+# const x = 1
+# const o = -1
 
-function wins(b_int, player)
-    # It's a win if any of the rows, columns, or diagonals add up to ±3
-    row_wins = count(sum(row) == 3 * player for row in eachrow(b_int))
-    col_wins = count(sum(col) == 3 * player for col in eachcol(b_int))
-    diag_wins = (sum(diag(b_int)) == 3 * player ? 1 : 0) + 
-                (sum(diag(rotr90(b_int))) == 3 * player ? 1 : 0)
-    (row_wins, col_wins, diag_wins)
-end
+# function wins(b_int, player)
+#     # It's a win if any of the rows, columns, or diagonals add up to ±3
+#     row_wins = count(sum(row) == 3 * player for row in eachrow(b_int))
+#     col_wins = count(sum(col) == 3 * player for col in eachcol(b_int))
+#     diag_wins = (sum(diag(b_int)) == 3 * player ? 1 : 0) + 
+#                 (sum(diag(rotr90(b_int))) == 3 * player ? 1 : 0)
+#     (row_wins, col_wins, diag_wins)
+# end
 
-function valid_double_win(wins)
-    rows, cols, diags = wins
-    sum(wins) == 2 && rows < 2 && cols < 2
-end
+# function valid_double_win(wins)
+#     rows, cols, diags = wins
+#     sum(wins) == 2 && rows < 2 && cols < 2
+# end
 
-function gamestate(board)
-    # Parse to Int Matrix
-    b_str = hcat(split.(board, "")...)
-    replace!(b_str, "X" => "1")
-    replace!(b_str, " " => "0")
-    replace!(b_str, "O" => "-1")
-    b_int = parse.(Int, b_str)
+# function gamestate(board)
+#     # Parse to Int Matrix
+#     b_str = hcat(split.(board, "")...)
+#     replace!(b_str, "X" => "1")
+#     replace!(b_str, " " => "0")
+#     replace!(b_str, "O" => "-1")
+#     b_int = parse.(Int, b_str)
 
-    sum(b_int) ∉ (0, 1) && error("Must take turns in order!")
+#     sum(b_int) ∉ (0, 1) && error("Must take turns in order!")
 
-    x_wins = wins(b_int, x)
-    o_wins = wins(b_int, o)
-    (sum(x_wins) > 0 && sum(o_wins) > 0) && error("Too many wins!")
+#     x_wins = wins(b_int, x)
+#     o_wins = wins(b_int, o)
+#     (sum(x_wins) > 0 && sum(o_wins) > 0) && error("Too many wins!")
 
-    total_wins = sum(x_wins) + sum(o_wins)
-    if total_wins == 1 || valid_double_win(x_wins) || valid_double_win(o_wins)
-        "win"
-    elseif total_wins > 1
-        error("Too many wins!")
-    elseif sum(b_int) == 1 && !any(x == 0 for x in b_int)
-        "draw"
-    else
-        "ongoing"
-    end
-end
+#     total_wins = sum(x_wins) + sum(o_wins)
+#     if total_wins == 1 || valid_double_win(x_wins) || valid_double_win(o_wins)
+#         "win"
+#     elseif total_wins > 1
+#         error("Too many wins!")
+#     elseif sum(b_int) == 1 && !any(x == 0 for x in b_int)
+#         "draw"
+#     else
+#         "ongoing"
+#     end
+# end
 
 # ========================================================================
 
