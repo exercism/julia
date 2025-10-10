842f1dd4656461f3e07a2d94bd74c3bba0c1e2c5
depial <91621102+depial@users.noreply.github.com>
2025-10-10 14:19:15 -0400

    add concept exercise cheese-club

---
+++ exercises/concept/cheese-club/.meta/exemplar.jl
@@ -0,0 +1,15 @@
+function all_15(ratings)
+    all(rating -> rating ∈ (1,5), ratings)
+end
+
+function emphatics(customers)
+    filter(customer -> all_15(customer[2]), customers)
+end
+
+function tobinary(ratings)
+    map(rating -> isone(rating) ? 0 : 1, ratings)
+end
+
+function tobinarymatrix(ratings)
+    mapreduce(transpose ∘ tobinary, vcat, ratings)
+end
