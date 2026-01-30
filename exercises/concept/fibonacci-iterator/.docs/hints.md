1c3a595abd61d476c8d3e73a4d36617fa0823a70
depial <91621102+depial@users.noreply.github.com>
2026-01-29 19:51:59 -0500

    update runtests.jl

--- exercises/concept/fibonacci-iterator/.docs/hints.md
+++ exercises/concept/fibonacci-iterator/.docs/hints.md
@@ -4,7 +4,7 @@
 
 - Read the Introduction, which steps through a similar example.
 
-## 1. Define a `Fib` type with a constructor that takes `n` as argument
+## 1. Define a `Fiberator` type with a constructor that takes `n` as argument
 
 - Keep it simple.
 - `n` is how many numbers to generate.
@@ -13,7 +13,7 @@
 
 - The `state` argument doesn't have to be an integer. It can be of any type.
 - Consider which values you need to calculate the next element. A tuple of those should be the `state` argument.
-- The `iterate` method returns a tuple with two values `(item, state)`. `item` is the Fibonacci number, i.e. the `item` in `for item in Fib(10)`.
+- The `iterate` method returns a tuple with two values `(item, state)`. `item` is the Fibonacci number, i.e. the `item` in `for item in Fiberator(10)`.
 - If your `state` includes several values, consider using a named tuple to clarify the meaning of each value.
   - This is useful but not obligatory.
 
--- exercises/concept/fibonacci-iterator/runtests.jl
+++ exercises/concept/fibonacci-iterator/runtests.jl
@@ -5,33 +5,32 @@ include("fibonacci.jl")
 const fib = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025]
 
 @testset verbose = true "tests" begin
-    @testset "Fiberator is defined" begin
+    @testset "1. Fiberator is defined" begin
         @test isdefined(@__MODULE__, :Fiberator)
         @test typeof(Fiberator(100)) == Fiberator
     end
 
-    @testset "Can be iterated" begin
+    @testset "2. Can be iterated" begin
         @test [a for a in Fiberator(50)] == fib
+        @testset "Fiberator does not contain fields to store state internally" begin
+            # Backport fieldtypes for Julia 1.0 - no longer relevant
+            # if VERSION < v"1.1"
+            #     @eval fieldtypes(T::Type) = ntuple(i -> fieldtype(T, i), fieldcount(T))
+            # end
+    
+            # Ensure only one numeric value (n) can be stored
+            # This should prevent Fiberator types that use a tuple or struct to store more state
+            @test fieldcount(Fiberator) == 1
+            @test fieldtypes(Fiberator)[1] <: Number
+        end
     end
 
-    @testset "Fib does not contain fields to store state internally" begin
-        # Backport fieldtypes for Julia 1.0 - no longer relevant
-        # if VERSION < v"1.1"
-        #     @eval fieldtypes(T::Type) = ntuple(i -> fieldtype(T, i), fieldcount(T))
-        # end
-
-        # Ensure only one numeric value (n) can be stored
-        # This should prevent Fiberator types that use a tuple or struct to store more state
-        @test fieldcount(Fiberator) == 1
-        @test fieldtypes(Fiberator)[1] <: Number
-    end
-
-    @testset "Can be collected" begin
+    @testset "3. Can be collected" begin
         @test collect(Fiberator(50)) == fib
         @test Base.IteratorSize(Fiberator) == Base.HasLength()
     end
 
-    @testset "Has an eltype" begin
+    @testset "4. Has an eltype" begin
         @test typeof(collect(Fiberator(50))) == Vector{Int}
         @test Base.IteratorEltype(Fiberator) == Base.HasEltype()
     end
