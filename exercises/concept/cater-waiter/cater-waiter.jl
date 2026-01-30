4ec17bc09d80a13ccd181bbd888bbf1af2ba5547
depial <91621102+depial@users.noreply.github.com>
2026-01-29 19:33:36 -0500

    update docs for cater-waiter (#1084)
    * update docs for cater-waiter
    
    * update hints.md
    
    * update constant

--- exercises/concept/cater-waiter/cater-waiter.jl
+++ exercises/concept/cater-waiter/cater-waiter.jl
@@ -1,27 +1,27 @@
 function clean_ingredients(dish_name, dish_ingredients)
-
+    
 end
 
 function check_drinks(drink_name, drink_ingredients)
-
+    
 end
 
 function categorize_dish(dish_name, dish_ingredients)
-
+    
 end
 
 function tag_special_ingredients(dish)
-
+    
 end
 
 function compile_ingredients(dishes)
-
+    
 end
 
 function separate_appetizers(dishes, appetizers)
-
+    
 end
 
 function singleton_ingredients(dishes, intersection)
-
+    
 end
